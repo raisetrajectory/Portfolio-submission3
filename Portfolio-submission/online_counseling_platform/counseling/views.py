f68586538a72a70eacad2a082e392a0e6b73be2d
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-21 15:43:20 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -2,6 +2,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.shortcuts import get_object_or_404
+
+# def counselor_profile(request, pk):
+#     counselor = get_object_or_404(Counselor, pk=pk)
+#     return render(request, 'counselor_profile.html', {'counselor': counselor})
+
+# def edit_counselor_profile(request, pk):
+#     counselor = get_object_or_404(Counselor, pk=pk)
+#     if request.method == 'POST':
+#         form = CounselorForm(request.POST, instance=counselor)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('counselor_profile', pk=counselor.pk)
+#     else:
+#         form = CounselorForm(instance=counselor)
+#     return render(request, 'edit_counselor_profile.html', {'form': form})
 
 def signup(request):
     if request.method == 'POST':
