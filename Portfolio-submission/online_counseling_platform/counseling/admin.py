ac4725ed83de9192e1b06b8c8c274a7408996b2a
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-21 15:17:15 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/admin.py
+++ Portfolio-submission/online_counseling_platform/counseling/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Counselor, CounselingSession, ChatMessage, Task
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Counselor)
+admin.site.register(CounselingSession)
+admin.site.register(ChatMessage)
+admin.site.register(Task)
