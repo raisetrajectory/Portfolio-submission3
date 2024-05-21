10c19f04352553424bf777d0b389657746490df0
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-21 15:32:34 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-def home(request):
-    return render(request, 'counseling/home.html')
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # ホームページにリダイレクト
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
