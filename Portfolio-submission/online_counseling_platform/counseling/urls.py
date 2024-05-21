6902d7049a614ccc8403ef2c1515d13fddfb74a3
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-21 15:31:11 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/urls.py
+++ Portfolio-submission/online_counseling_platform/counseling/urls.py
@@ -1,7 +1,11 @@
+# counseling/urls.py
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'), # type: ignore
-    # 他のURLパターンをここに追加
+    path('signup/', views.signup, name='signup'), # type: ignore
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # 他のURLパターン
 ]
