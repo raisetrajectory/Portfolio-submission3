248e1b3fd815801d1872f282a518a02b455a20bd
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-21 14:50:37 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/models.py
+++ Portfolio-submission/online_counseling_platform/counseling/models.py
@@ -7,14 +7,14 @@ class User(AbstractUser):
     # Add related_name to avoid clashes
     groups = models.ManyToManyField(
         'auth.Group',
-        related_name='custom_user_set',  # Change this line
+        related_name='counseling_user_set',  # Changed this line
         blank=True,
         help_text='The groups this user belongs to.',
         verbose_name='groups',
     )
     user_permissions = models.ManyToManyField(
         'auth.Permission',
-        related_name='custom_user_set',  # Change this line
+        related_name='counseling_user_set',  # Changed this line
         blank=True,
         help_text='Specific permissions for this user.',
         verbose_name='user permissions',
