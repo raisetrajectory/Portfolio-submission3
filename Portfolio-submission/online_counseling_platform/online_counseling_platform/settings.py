ae78dde7a5c4b33e25518f92ebd177ba149511bc
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-22 19:27:39 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/online_counseling_platform/settings.py
+++ Portfolio-submission/online_counseling_platform/online_counseling_platform/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = "online_counseling_platform.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [TEMPLATE_DIR,],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
