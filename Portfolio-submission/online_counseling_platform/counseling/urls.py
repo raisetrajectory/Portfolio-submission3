3bc7e752c5935fb848117c6f0864170f63fdf474
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-22 18:23:35 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/urls.py
+++ Portfolio-submission/online_counseling_platform/counseling/urls.py
@@ -6,10 +6,10 @@ from .views import create_session, chat_view, send_message, session_detail # typ
 
 urlpatterns = [
     path('', views.home, name='home'),  # ホームビューのルーティング # type: ignore
-    path('counselor/<int:pk>/', views.counselor_profile, name='counselor_profile'), # type: ignore
-    path('counselor/<int:pk>/edit/', views.edit_counselor_profile, name='edit_counselor_profile'), # type: ignore
     path('signup/', views.signup, name='signup'), # type: ignore
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('counselor/<int:pk>/', views.counselor_profile, name='counselor_profile'), # type: ignore
+    path('counselor/<int:pk>/edit/', views.edit_counselor_profile, name='edit_counselor_profile'), # type: ignore
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('create_session/', create_session, name='create_session'),
     path('chat/<int:session_id>/', chat_view, name='chat_view'),
