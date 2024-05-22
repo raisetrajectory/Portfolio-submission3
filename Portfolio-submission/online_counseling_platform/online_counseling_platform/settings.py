7f5f80655acf986f41028d1d1743f627069c2c98
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-22 10:31:56 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/online_counseling_platform/settings.py
+++ Portfolio-submission/online_counseling_platform/online_counseling_platform/settings.py
@@ -136,7 +136,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+# STATICFILES_DIRSは、追加の静的ファイルディレクトリがある場合に指定します
+# 例えば、プロジェクト全体で共有される静的ファイルなど
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# 静的ファイルの収集先
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
