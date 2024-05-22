5d845dda604393843bbe8cf048ceb1b5b1fdb44f
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-22 09:32:19 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -2,8 +2,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.shortcuts import get_object_or_404
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Counselor
