555f1d889c4c9b93e6d64e72dad4bc876006c9a1
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-23 13:04:47 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/online_counseling_platform/urls.py
+++ Portfolio-submission/online_counseling_platform/online_counseling_platform/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Allauthのルーティングを追加
     path('', include('counseling.urls')),  # counselingアプリのルーティングを追加
+    path('logout/', LogoutView.as_view(), name='logout'),  # ログアウト機能の追加
 ]
