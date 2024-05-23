da4bca6b5c0ad3412cc77da865491a497c602c9b
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-23 14:44:48 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/urls.py
+++ Portfolio-submission/online_counseling_platform/counseling/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('chat/', views.chat_view, name='chat'),  # チャット画面のURL
     path('logout/', views.logout_view, name='logout'),  # ログアウト機能の追加
-    # path('profile/', views.profile_view, name='profile'),  # プロフィール画面の追加 # type: ignore
+    path('profile/', views.profile_view, name='profile'),  # プロフィール画面の追加 # type: ignore
     path('counselor/<int:pk>/', views.counselor_profile, name='counselor_profile'), # type: ignore
     path('counselor/<int:pk>/edit/', views.edit_counselor_profile, name='edit_counselor_profile'), # type: ignore
     path('create_session/', create_session, name='create_session'),
@@ -19,7 +19,6 @@ urlpatterns = [
     path('session/<int:session_id>/', session_detail, name='session_detail'),
 
     # path('register/', views.register, name='register'), # type: ignore
-    # path('chat/', views.chat, name='chat'), # type: ignore
     # path('profile/', views.profile, name='profile'), # type: ignore
     # path('counselor-list/', views.counselor_list, name='counselor_list'), # type: ignore
 ]
