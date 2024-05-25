5f982c2eb07d3eed3c54b4440b50bcdeb07edfb3
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-25 11:54:43 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/main_app/views.py
+++ Portfolio-submission/online_counseling_platform/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import ChatMessage  # チャットメッセージのモデルをインポート
 from django.template.loader import get_template
