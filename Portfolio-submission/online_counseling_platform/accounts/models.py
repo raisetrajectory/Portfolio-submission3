4c940f97527ae5dda95be3307ca018d5a6baad58
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-26 13:05:34 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/accounts/models.py
+++ Portfolio-submission/online_counseling_platform/accounts/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
