17cdc8395236eacee9b786d1fad709cac204b588
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-26 17:27:37 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -8,22 +8,7 @@ class CounselorForm(forms.ModelForm):
         model = Counselor
         fields = ['bio']  # 他の必要なフィールドを追加
 
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = User  # Userモデルを使用
-#         fields = ['username', 'email', 'profile_picture']
-
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = Counselor  # Counselor モデルを使用する
-#         fields = ['bio', 'profile_picture']  # プロフィール情報に対応するフィールドを追加
-
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = Counselor
-#         fields = ['picture']
-
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Counselor
-        fields = ['name', 'age', 'grade', 'profile_picture']  # profile_pictureを含める
+        fields = ['bio']  # 'bio'フィールドのみを含める
