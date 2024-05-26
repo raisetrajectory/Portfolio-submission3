4605d386ffc5aa127c949c94de85ccb7da13e409
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-26 16:30:38 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/accounts/models.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -1,7 +1,7 @@
 # counseling/forms.py
 from django import forms
 from .models import Counselor
-from accounts.models import CustomUser  # CustomUserをインポート
+from accounts.models import CustomUser # type: ignore
 from django.contrib.auth.models import User
 
 class CounselorForm(forms.ModelForm):
@@ -9,12 +9,12 @@ class CounselorForm(forms.ModelForm):
         model = Counselor
         fields = ['bio']  # 他の必要なフィールドを追加
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser  # UserではなくCustomUserを使用
-        fields = ['username', 'email', 'profile_picture']
-
 # class ProfileForm(forms.ModelForm):
 #     class Meta:
-#         model = User  # Userモデルを使用
+#         model = CustomUser  # UserではなくCustomUserを使用
 #         fields = ['username', 'email', 'profile_picture']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User  # Userモデルを使用
+        fields = ['username', 'email', 'profile_picture']
