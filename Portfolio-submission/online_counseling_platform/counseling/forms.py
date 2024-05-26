d12c43d84d8f966618ee0b4cbffead6cd1b8df44
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-26 17:14:34 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -1,18 +1,12 @@
 # counseling/forms.py
 from django import forms
 from .models import Counselor
-# from accounts.models import CustomUser
 from django.contrib.auth.models import User
 
 class CounselorForm(forms.ModelForm):
     class Meta:
         model = Counselor
         fields = ['bio']  # 他の必要なフィールドを追加
-
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = CustomUser  # UserではなくCustomUserを使用
-#         fields = ['username', 'email', 'profile_picture']
 
 # class ProfileForm(forms.ModelForm):
 #     class Meta:
