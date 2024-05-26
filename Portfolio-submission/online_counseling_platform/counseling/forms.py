3829e558cbf5b4c9e968cd1c3087f4dfd02c1c67
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-26 10:27:31 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -1,8 +1,14 @@
 # counseling/forms.py
 from django import forms
 from .models import Counselor
+from .models import User
 
 class CounselorForm(forms.ModelForm):
     class Meta:
         model = Counselor
         fields = ['bio']  # 他の必要なフィールドを追加
+
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ['profile_picture']
