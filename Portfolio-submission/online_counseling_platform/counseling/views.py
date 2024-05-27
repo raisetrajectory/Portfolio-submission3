58efc2accbeb11511ec4d5e7d54291c19a87d55d
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-27 17:29:49 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -58,12 +58,22 @@ def edit_counselor_profile(request, pk):
         form = CounselorForm(instance=counselor)
     return render(request, 'edit_counselor_profile.html', {'form': form})
 
+# def signup(request):
+#     if request.method == 'POST':
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             form.save()
+#             return redirect('home')  # ホームページにリダイレクト
+#     else:
+#         form = CustomUserCreationForm()
+#     return render(request, 'signup.html', {'form': form})
+
 def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
             form.save()
             return redirect('home')  # ホームページにリダイレクト
     else:
