348406849acb8025de1c6038982496a1d5525c20
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-27 17:35:02 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -22,7 +22,6 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
         labels = {
             'username': 'ユーザーネーム',
-            'email': 'メールアドレス',
         }
 
 class CustomAuthenticationForm(AuthenticationForm):
