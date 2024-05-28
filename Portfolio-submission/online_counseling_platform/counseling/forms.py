1fe1033e63883542cbf3c8dd722a3d231c6d825e
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-28 12:33:40 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -34,3 +34,32 @@ class ChatMessageForm(forms.ModelForm):
         labels = {
             'content': 'メッセージ',
         }
+
+# # counseling/forms.py
+# from django import forms
+# from .models import Counselor
+# from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+# class CounselorForm(forms.ModelForm):
+#     class Meta:
+#         model = Counselor
+#         fields = ['bio']  # 他の必要なフィールドを追加
+
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Counselor
+#         fields = ['bio']  # 'bio'フィールドのみを含める
+
+# class CustomUserCreationForm(UserCreationForm):
+#     email = forms.EmailField(max_length=150, help_text='必須。有効なメールアドレスを入力してください。')
+
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password1', 'password2')
+#         labels = {
+#             'username': 'ユーザーネーム',
+#         }
+
+# class CustomAuthenticationForm(AuthenticationForm):
+#     username = forms.CharField(label='ユーザーネームまたはメールアドレス')
