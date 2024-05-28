c59194b715a12c7eb6deaf51b13078c86f3f6958
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-28 15:32:44 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -1,3 +1,40 @@
+# # counseling/forms.py
+# from django import forms
+# from .models import Counselor
+# # from .models import Counselor, ChatMessage  # ChatMessage を追加
+# from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+# class CounselorForm(forms.ModelForm):
+#     class Meta:
+#         model = Counselor
+#         fields = ['bio']  # 他の必要なフィールドを追加
+
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Counselor
+#         fields = ['bio']  # 'bio'フィールドのみを含める
+
+# class CustomUserCreationForm(UserCreationForm):
+#     email = forms.EmailField(max_length=150, help_text='必須。有効なメールアドレスを入力してください。')
+
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password1', 'password2')
+#         labels = {
+#             'username': 'ユーザーネーム',
+#         }
+
+# class CustomAuthenticationForm(AuthenticationForm):
+#     username = forms.CharField(label='ユーザーネームまたはメールアドレス')
+
+# # class ChatMessageForm(forms.ModelForm):  # ChatMessageForm を追加
+# #     class Meta:
+# #         model = ChatMessage
+# #         fields = ['content']
+
+# 1~34の記載内容に関しては、現段階で問題無い記載となっております。バックアップ保存用です！
+
 # counseling/forms.py
 from django import forms
 from .models import Counselor
@@ -32,5 +69,3 @@ class CustomAuthenticationForm(AuthenticationForm):
 #     class Meta:
 #         model = ChatMessage
 #         fields = ['content']
-
-# 1~34の記載内容に関しては、現段階で問題無い記載となっております。バックアップ保存用です！
