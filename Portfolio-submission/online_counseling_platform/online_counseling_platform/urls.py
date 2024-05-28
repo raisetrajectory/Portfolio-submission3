5bb3a50df8260d58d033c834beee828de9151f7b
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-28 11:05:23 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/online_counseling_platform/urls.py
+++ Portfolio-submission/online_counseling_platform/online_counseling_platform/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Allauthのルーティングを追加
