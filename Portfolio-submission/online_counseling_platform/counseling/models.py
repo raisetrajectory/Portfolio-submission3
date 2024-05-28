d3dd633b9169067543dab5b808b3917a5f930b93
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-28 11:49:31 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/models.py
+++ Portfolio-submission/online_counseling_platform/counseling/models.py
@@ -39,6 +39,12 @@ class ChatMessage(models.Model):
     def __str__(self):
         return f'{self.sender.username}: {self.content}'
 
+# class ChatMessage(models.Model):
+#     session = models.ForeignKey(CounselingSession, on_delete=models.CASCADE)
+#     sender = models.ForeignKey(User, on_delete=models.CASCADE)
+#     message = models.TextField()
+#     timestamp = models.DateTimeField(auto_now_add=True)
+
 class Task(models.Model):
     ASSIGNED_ROLE_CHOICES = [
         ('user', 'User'),
