144827976a3faa888faf020535c99caf2beb2266
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-28 13:03:53 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -3,6 +3,12 @@ from django import forms
 from .models import Counselor
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+# from .models import ChatMessage
+
+# class ChatMessageForm(forms.ModelForm):
+#     class Meta:
+#         model = ChatMessage
+#         fields = ['content']
 
 class CounselorForm(forms.ModelForm):
     class Meta:
