81e4a02284082611d9e71f7f286d5a11e4f3c08e
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-05-29 13:00:07 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -144,6 +144,7 @@ from .forms import CustomUserCreationForm, CustomAuthenticationForm, CounselorFo
 from .models import Counselor, CounselingSession, ChatMessage
 from django.contrib.auth import get_user_model
 from django.utils.functional import SimpleLazyObject
+from django.contrib.auth.models import AbstractBaseUser
 
 def home(request):
     return render(request, 'home.html')
@@ -451,6 +452,24 @@ def get_messages(request):
 #                 return redirect('chat_view', session_id=session_id)
 #     return redirect('home')
 
+# @login_required
+# def send_message(request):
+#     if request.method == 'POST':
+#         form = ChatMessageForm(request.POST)
+#         if form.is_valid():
+#             chat_message = form.save(commit=False)
+#             user = request.user
+#             if isinstance(user, SimpleLazyObject):
+#                 user = user._wrapped
+#             chat_message.sender = user
+#             session_id = request.POST.get('session_id')
+#             if session_id:
+#                 chat_message.session = get_object_or_404(CounselingSession, id=session_id)
+#             chat_message.save()
+#             if session_id:
+#                 return redirect('chat_view', session_id=session_id)
+#     return redirect('home')
+
 @login_required
 def send_message(request):
     if request.method == 'POST':
@@ -458,9 +477,8 @@ def send_message(request):
         if form.is_valid():
             chat_message = form.save(commit=False)
             user = request.user
-            if isinstance(user, SimpleLazyObject):
-                user = user._wrapped
-            chat_message.sender = user
+            if isinstance(user, AbstractBaseUser):
+                chat_message.sender = user
             session_id = request.POST.get('session_id')
             if session_id:
                 chat_message.session = get_object_or_404(CounselingSession, id=session_id)
