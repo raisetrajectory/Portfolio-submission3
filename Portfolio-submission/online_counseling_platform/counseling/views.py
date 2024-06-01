811d8db5fa29033b9e65e24ee418e0f3bea62681
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-01 19:43:24 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -358,7 +358,7 @@ def send_message(request):
 #     session_id = message.session.id
 #     if request.user == message.sender:
 #         message.delete()
-#     return redirect('chat_view', session_id=session_id)
+#     return redirect('counseling/registration/chat.html', session_id=session_id)
 
 @login_required
 def delete_message(request, message_id):
