2cb1363a8c4c582781b82edc5dc93fa0e3111038
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-01 10:57:49 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/models.py
+++ Portfolio-submission/online_counseling_platform/counseling/models.py
@@ -54,8 +54,6 @@
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model() # type: ignore
