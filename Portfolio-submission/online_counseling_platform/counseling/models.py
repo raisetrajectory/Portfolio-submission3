afbad73febf094aead2a6e3cf571860b6edba4f7
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-01 10:00:02 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/models.py
+++ Portfolio-submission/online_counseling_platform/counseling/models.py
@@ -55,6 +55,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from django.db import models
+from django.contrib.auth import get_user_model
+
 class User(AbstractUser):
     is_counselor = models.BooleanField(default=False)
 
