5dd328ec036123fd010e85865519248d026a89cb
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-04 18:29:00 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/urls.py
+++ Portfolio-submission/online_counseling_platform/counseling/urls.py
@@ -59,7 +59,6 @@ urlpatterns = [
     path('chat/counselor/<int:counselor_id>/', views.chat_view, name='chat_counselor'),
     path('send_message/', send_message, name='send_message'),
     path('chat/<int:session_id>/', chat_view, name='chat_view'),
-    # path('delete_message/<int:message_id>/', delete_message, name='delete_message'),
     path('delete_message/<int:message_id>/', views.delete_message, name='delete_message'),
 
     path('chat/', views.chat_view, name='chat_view'),
