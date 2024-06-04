bd75ad50758fc1ce8b1d23bd591426ed02a7e1a3
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-04 10:35:28 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/models.py
+++ Portfolio-submission/online_counseling_platform/counseling/models.py
@@ -59,6 +59,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model() # type: ignore
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
 class User(AbstractUser):
     is_counselor = models.BooleanField(default=False)
 
