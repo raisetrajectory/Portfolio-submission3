803064cb61364fb73082df0fe97a20c5018fd6d8
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-06 08:27:36 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -159,16 +159,27 @@ from django.urls import reverse #2024年6月5日追加
 from .forms import UploadFileForm #2024年6月6日追加
 from django.conf import settings #2024年6月6日追加
 
-def upload_sample(request): #2024年6月5日追加
+def upload_sample(request): #2024年6月6日追加
     if request.method == 'POST' and request.FILES['upload_file']:
-        # 送られたファイルの取り出し
         upload_file = request.FILES['upload_file']
-        fs = FileSystemStorage() # ファイルを保存する
-        file_path = os.path.join('upload', upload_file.name)
-        file = fs.save(file_path, upload_file)
-        uploaded_file_url = fs.url(file)
+        fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+        file_path = fs.save(upload_file.name, upload_file)
+        uploaded_file_url = fs.url(file_path)
         return HttpResponseRedirect(reverse('profile') + '?uploaded_file_url=' + uploaded_file_url)
-    return render(request, 'online_counseling_platform/profile.html')
+    else:
+        form = UploadFileForm()
+    return render(request, 'online_counseling_platform/profile.html', {'form': form})
+
+# def upload_sample(request): #2024年6月5日追加
+#     if request.method == 'POST' and request.FILES['upload_file']:
+#         # 送られたファイルの取り出し
+#         upload_file = request.FILES['upload_file']
+#         fs = FileSystemStorage() # ファイルを保存する
+#         file_path = os.path.join('upload', upload_file.name)
+#         file = fs.save(file_path, upload_file)
+#         uploaded_file_url = fs.url(file)
+#         return HttpResponseRedirect(reverse('profile') + '?uploaded_file_url=' + uploaded_file_url)
+#     return render(request, 'online_counseling_platform/profile.html')
 
 # def upload_sample(request): #2024年6月5日追加
 #     if request.method == 'POST' and request.FILES['upload_file']:
