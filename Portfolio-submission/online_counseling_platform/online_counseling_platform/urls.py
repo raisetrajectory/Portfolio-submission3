d49c64591deb96b54f41c1b50014e1e67c2fe8de
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-06 17:18:34 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/online_counseling_platform/urls.py
+++ Portfolio-submission/online_counseling_platform/online_counseling_platform/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),  # Allauthのルーティングを追加
     path('', include('counseling.urls')),  # counselingアプリのルーティングを追加
     path('logout/', LogoutView.as_view(), name='logout'),  # ログアウト機能の追加
+    path('counseling/', include('counseling.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
