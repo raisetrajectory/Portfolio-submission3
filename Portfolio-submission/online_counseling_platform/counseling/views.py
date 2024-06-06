ff1a9989412487a63a4e4d5e9d845c6c389910ae
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-06 17:48:34 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -217,17 +217,25 @@ User = get_user_model()
 #         return redirect('profile')
 #     return render(request, 'profile.html')
 
-def profile(request): #2024年6月6日追加
-    if request.method == 'POST' and request.FILES.get('upload_file'):
-        upload_file = request.FILES['upload_file']
-        fs = FileSystemStorage()
-        file_path = fs.save(upload_file.name, upload_file)
-        uploaded_file_url = fs.url(file_path)
-        request.session['uploaded_file_url'] = uploaded_file_url  # セッションに保存
-        print("uploaded_file_url:", uploaded_file_url)  # ログに出力
-        return redirect('profile')
-    return render(request, 'profile.html')
+# def profile(request): #2024年6月6日追加
+#     if request.method == 'POST' and request.FILES.get('upload_file'):
+#         upload_file = request.FILES['upload_file']
+#         fs = FileSystemStorage()
+#         file_path = fs.save(upload_file.name, upload_file)
+#         uploaded_file_url = fs.url(file_path)
+#         request.session['uploaded_file_url'] = uploaded_file_url  # セッションに保存
+#         print("uploaded_file_url:", uploaded_file_url)  # ログに出力
+#         return redirect('profile')
+#     return render(request, 'profile.html')
 
+def profile(request): #2024年6月6日追加
+    if request.method == 'POST' and request.FILES['upload_file']:
+        uploaded_file = request.FILES['upload_file']
+        fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+        filename = fs.save(uploaded_file.name, uploaded_file)
+        uploaded_file_url = fs.url(filename)
+        request.session['uploaded_file_url'] = uploaded_file_url
+    return render(request, 'profile.html', {'user': request.user})
 
 # def upload_sample(request): #2024年6月6日追加
 #     if request.method == 'POST' and request.FILES['upload_file']:
