2db842886cb4195d2641cf1825cbd200f693ffc8
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-07 13:24:22 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -268,19 +268,6 @@ def session_detail(request, session_id):
 #         'user': request.user,
 #     })
 
-# @login_required
-# def send_message(request):
-#     if request.method == 'POST':
-#         form = ChatMessageForm(request.POST)
-#         if form.is_valid():
-#             session_id = form.cleaned_data['session_id']
-#             session = get_object_or_404(CounselingSession, id=session_id)
-#             message_text = form.cleaned_data['message']
-#             chat_message = ChatMessage(sender=request.user, message=message_text, session=session)
-#             chat_message.save()
-#             return redirect('chat_view', session_id=session.id)  # 正しいリダイレクトURLを設定
-#     return redirect('home')
-
 @login_required #2024年6月7日追加
 def send_message(request):
     if request.method == 'POST':
