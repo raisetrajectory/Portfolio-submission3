f9709f26ee3f97df5e07361c7eeb6d85b48cc163
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-08 14:16:19 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -358,27 +358,6 @@ def chat_view(request, session_id=None, counselor_id=None):
         'user': request.user,
     })
 
-
-# @login_required #2024年6月7日追加
-# def chat_view(request, session_id=None, counselor_id=None):
-#     session = None
-#     if session_id:
-#         session = get_object_or_404(CounselingSession, id=session_id)
-#     elif counselor_id:
-#         counselor = get_object_or_404(Counselor, id=counselor_id)
-#         session, created = CounselingSession.objects.get_or_create(user=request.user, counselor=counselor)
-
-#     # フォームに初期値を設定
-#     form = ChatMessageForm(initial={'session': session.id}) if session else ChatMessageForm()
-#     messages = ChatMessage.objects.filter(session=session).order_by('timestamp') if session else []
-
-#     return render(request, 'counseling/registration/chat.html', {
-#         'form': form,
-#         'messages': messages,
-#         'session': session,
-#         'user': request.user,
-#     })
-
 @login_required
 def create_session(request):
     if request.method == 'POST':
