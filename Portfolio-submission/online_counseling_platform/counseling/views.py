af57e5df992a0d6fe228ce23e814fe1a9171bb29
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-09 19:48:59 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -239,20 +239,7 @@ def session_detail(request, session_id):
 #         'user': request.user,
 #     })
 
-# @login_required #2024年6月9日追加
-# def send_message(request):
-#     if request.method == 'POST':
-#         form = ChatMessageForm(request.POST)
-#         if form.is_valid():
-#             chat_message = form.save(commit=False)
-#             chat_message.sender = request.user
-#             chat_message.session = get_object_or_404(CounselingSession, id=form.cleaned_data['session_id'])
-#             chat_message.save()
-#             # メッセージを保存した後に直接チャット画面を表示する
-#             return redirect('chat_view', session_id=form.cleaned_data['session_id'])
-#     return redirect('chat_view') #コメントを入力後にチャット画面にリダイレクト出来ております！
-
-@login_required
+@login_required #2024年6月9日追加
 def send_message(request):
     if request.method == 'POST':
         form = ChatMessageForm(request.POST)
@@ -261,9 +248,22 @@ def send_message(request):
             chat_message.sender = request.user
             chat_message.session = get_object_or_404(CounselingSession, id=form.cleaned_data['session_id'])
             chat_message.save()
-            # チャット画面にリダイレクトする際に、session_idと一緒にchat_messageのidを渡す
-            return redirect('chat_view', session_id=form.cleaned_data['session_id'], chat_message_id=chat_message.id)
-    return redirect('chat_view')
+            # メッセージを保存した後に直接チャット画面を表示する
+            return redirect('chat_view', session_id=form.cleaned_data['session_id'])
+    return redirect('chat_view') #コメントを入力後にチャット画面にリダイレクト出来ております！
+
+# @login_required
+# def send_message(request):
+#     if request.method == 'POST':
+#         form = ChatMessageForm(request.POST)
+#         if form.is_valid():
+#             chat_message = form.save(commit=False)
+#             chat_message.sender = request.user
+#             chat_message.session = get_object_or_404(CounselingSession, id=form.cleaned_data['session_id'])
+#             chat_message.save()
+#             # チャット画面にリダイレクトする際に、session_idと一緒にchat_messageのidを渡す
+#             return redirect('chat_view', session_id=form.cleaned_data['session_id'], chat_message_id=chat_message.id)
+#     return redirect('chat_view')
 
 
 @login_required
