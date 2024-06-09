a0589c241393aa81db29fb1a4eca1a7a6510790b
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-09 15:34:10 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -376,34 +376,6 @@ def chat_view(request, session_id=None, counselor_id=None):
         'user': request.user,
     })
 
-# @login_required
-# def chat_view(request, session_id=None, counselor_id=None):
-#     session = None
-#     if session_id:
-#         session = get_object_or_404(CounselingSession, id=session_id)
-#     elif counselor_id:
-#         counselor = get_object_or_404(Counselor, id=counselor_id)
-#         session, _ = CounselingSession.objects.get_or_create(user=request.user, counselor=counselor)
-
-#     if request.method == 'POST':
-#         form = ChatMessageForm(request.POST)
-#         if form.is_valid():
-#             chat_message = form.save(commit=False)
-#             chat_message.sender = get_user_model().objects.get(pk=request.user.pk)  # Userインスタンスを明示的に取得する
-#             chat_message.session = session
-#             chat_message.save()
-#             return redirect('chat_view', session_id=session.id)
-
-#     form = ChatMessageForm(initial={'session_id': session.id if session else None})
-#     messages = ChatMessage.objects.filter(session=session).order_by('timestamp') if session else []
-
-#     return render(request, 'counseling/registration/chat.html', {
-#         'form': form,
-#         'messages': messages,
-#         'session': session,
-#         'user': request.user,
-#     })
-
 @login_required
 def create_session(request):
     if request.method == 'POST':
