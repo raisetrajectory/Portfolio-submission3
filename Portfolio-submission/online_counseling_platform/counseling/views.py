60abf3479f44d9b6272d6ae45251a978939100da
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-09 19:56:40 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -239,18 +239,18 @@ def session_detail(request, session_id):
 #         'user': request.user,
 #     })
 
-@login_required #2024年6月9日追加
-def send_message(request):
-    if request.method == 'POST':
-        form = ChatMessageForm(request.POST)
-        if form.is_valid():
-            chat_message = form.save(commit=False)
-            chat_message.sender = request.user
-            chat_message.session = get_object_or_404(CounselingSession, id=form.cleaned_data['session_id'])
-            chat_message.save()
-            # メッセージを保存した後に直接チャット画面を表示する
-            return redirect('chat_view', session_id=form.cleaned_data['session_id'])
-    return redirect('chat_view') #コメントを入力後にチャット画面にリダイレクト出来ております！
+# @login_required #2024年6月9日追加
+# def send_message(request):
+#     if request.method == 'POST':
+#         form = ChatMessageForm(request.POST)
+#         if form.is_valid():
+#             chat_message = form.save(commit=False)
+#             chat_message.sender = request.user
+#             chat_message.session = get_object_or_404(CounselingSession, id=form.cleaned_data['session_id'])
+#             chat_message.save()
+#             # メッセージを保存した後に直接チャット画面を表示する
+#             return redirect('chat_view', session_id=form.cleaned_data['session_id'])
+#     return redirect('chat_view') #コメントを入力後にチャット画面にリダイレクト出来ております！
 
 # @login_required
 # def send_message(request):
@@ -265,6 +265,23 @@ def send_message(request):
 #             return redirect('chat_view', session_id=form.cleaned_data['session_id'], chat_message_id=chat_message.id)
 #     return redirect('chat_view')
 
+messages = []  # メッセージの初期化
+
+@login_required
+def send_message(request):
+    global messages  # グローバル変数として定義された messages を使用する
+
+    if request.method == 'POST':
+        form = ChatMessageForm(request.POST)
+        if form.is_valid():
+            chat_message = form.save(commit=False)
+            chat_message.sender = request.user
+            chat_message.session = get_object_or_404(CounselingSession, id=form.cleaned_data['session_id'])
+            chat_message.save()
+            # 保存したメッセージをリストに追加する
+            messages = [chat_message]
+            return redirect('chat_view', session_id=form.cleaned_data['session_id'])
+    return redirect('chat_view')
 
 @login_required
 def delete_message(request, message_id):
