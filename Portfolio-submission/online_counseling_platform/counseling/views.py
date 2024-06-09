5a8444746810b959beece09d23d1c5a962662325
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-09 17:34:53 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -145,6 +145,7 @@ from .models import Counselor, CounselingSession, ChatMessage
 from django.contrib.auth import get_user_model
 from django.utils.functional import SimpleLazyObject
 from django.urls import reverse
+from django.contrib import messages
 
 import os #2024年6月5日追加
 from django.core.files.storage import FileSystemStorage #2024年6月5日追加
@@ -153,8 +154,6 @@ from .forms import UploadFileForm #2024年6月6日追加
 from django.conf import settings #2024年6月6日追加
 
 User = get_user_model()
-
-from django.contrib import messages
 
 # from .models import Profile #2024年6月6日追加
 
