c087ca9b2e744c350da7eeede89e113be4b61276
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-11 08:28:01 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -155,6 +155,19 @@ class CustomAuthenticationForm(AuthenticationForm):
 #         if session_id:
 #             self.fields['session_id'].initial = session_id
 
+# class ChatMessageForm(forms.ModelForm):
+#     session_id = forms.IntegerField(widget=forms.HiddenInput())
+
+#     class Meta:
+#         model = ChatMessage
+#         fields = ['message', 'session_id']
+
+#     def __init__(self, *args, **kwargs):
+#         session_id = kwargs.pop('session_id', None)
+#         super().__init__(*args, **kwargs)
+#         if session_id:
+#             self.fields['session_id'].initial = session_id
+
 class ChatMessageForm(forms.ModelForm):
     session_id = forms.IntegerField(widget=forms.HiddenInput())
 
