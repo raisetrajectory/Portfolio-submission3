e88329c053b12bc935f8d6b75c67cbb70be16bf0
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-10 17:14:38 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -583,6 +583,36 @@ def delete_message(request, message_id):
 #         'user': request.user,
 #     })
 
+# @login_required
+# def chat_view(request, session_id=None, counselor_id=None):
+#     session = None
+#     if session_id:
+#         session = get_object_or_404(CounselingSession, id=session_id)
+#     elif counselor_id:
+#         counselor = get_object_or_404(Counselor, id=counselor_id)
+#         session, _ = CounselingSession.objects.get_or_create(user=request.user, counselor=counselor)
+
+#     if request.method == 'POST':
+#         form = ChatMessageForm(request.POST, session_id=session_id)
+#         if form.is_valid():
+#             message = form.cleaned_data['message']
+#             form.instance.session_id = session_id
+#             form.instance.sender = request.user  # 送信者を設定
+#             form.save()
+#             form.save(session_id=session_id)  # セッションIDを渡して保存
+#             return redirect('chat_view', session_id=session_id)  # チャット画面にリダイレクト
+#     else:
+#         form = ChatMessageForm(initial={'session_id': session_id})
+
+#     messages = ChatMessage.objects.filter(session=session).order_by('timestamp') if session else []
+
+#     return render(request, 'counseling/registration/chat.html', {
+#         'form': form,
+#         'messages': messages,
+#         'session': session,
+#         'user': request.user,
+#     })
+
 @login_required
 def chat_view(request, session_id=None, counselor_id=None):
     session = None
@@ -597,9 +627,8 @@ def chat_view(request, session_id=None, counselor_id=None):
         if form.is_valid():
             message = form.cleaned_data['message']
             form.instance.session_id = session_id
-            form.instance.sender = request.user  # 送信者を設定
+            form.instance.sender_id = request.user.id  # sender_id に User の ID を代入
             form.save()
-            form.save(session_id=session_id)  # セッションIDを渡して保存
             return redirect('chat_view', session_id=session_id)  # チャット画面にリダイレクト
     else:
         form = ChatMessageForm(initial={'session_id': session_id})
@@ -612,7 +641,6 @@ def chat_view(request, session_id=None, counselor_id=None):
         'session': session,
         'user': request.user,
     })
-
 
 @login_required
 def create_session(request):
