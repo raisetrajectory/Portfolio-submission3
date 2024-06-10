343205ed07b3d9f8bc41c6c45448a87c4350c267
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-11 07:59:27 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -811,7 +811,40 @@ def delete_message(request, message_id):
 #         'user': request.user,
 #     })
 
-@login_required #2024年6月11日追加
+# @login_required #2024年6月11日追加
+# def chat_view(request, session_id=None, counselor_id=None):
+#     session = None
+#     if session_id:
+#         session = get_object_or_404(CounselingSession, id=session_id)
+#     elif counselor_id:
+#         counselor = get_object_or_404(Counselor, id=counselor_id)
+#         session, _ = CounselingSession.objects.get_or_create(user=request.user, counselor=counselor)
+
+#     initial = {'session_id': session.id} if session else None
+
+#     if request.method == 'POST':
+#         form = ChatMessageForm(request.POST, initial=initial)
+#         if form.is_valid():
+#             message = form.save(commit=False)
+#             message.sender = request.user
+#             message.session = session
+#             message.save()
+#             return redirect('chat_view', session_id=session.id)  # チャット画面にリダイレクト
+#         else:
+#             print(form.errors)  # フォームのエラーをデバッグ出力
+#     else:
+#         form = ChatMessageForm(initial=initial)
+
+#     messages = ChatMessage.objects.filter(session=session).order_by('timestamp') if session else []
+
+#     return render(request, 'counseling/registration/chat.html', {
+#         'form': form,
+#         'messages': messages,
+#         'session': session,
+#         'user': request.user,
+#     })
+
+@login_required
 def chat_view(request, session_id=None, counselor_id=None):
     session = None
     if session_id:
@@ -820,10 +853,8 @@ def chat_view(request, session_id=None, counselor_id=None):
         counselor = get_object_or_404(Counselor, id=counselor_id)
         session, _ = CounselingSession.objects.get_or_create(user=request.user, counselor=counselor)
 
-    initial = {'session_id': session.id} if session else None
-
     if request.method == 'POST':
-        form = ChatMessageForm(request.POST, initial=initial)
+        form = ChatMessageForm(request.POST, session_id=session.id if session else None)
         if form.is_valid():
             message = form.save(commit=False)
             message.sender = request.user
@@ -833,7 +864,7 @@ def chat_view(request, session_id=None, counselor_id=None):
         else:
             print(form.errors)  # フォームのエラーをデバッグ出力
     else:
-        form = ChatMessageForm(initial=initial)
+        form = ChatMessageForm(session_id=session.id if session else None)
 
     messages = ChatMessage.objects.filter(session=session).order_by('timestamp') if session else []
 
