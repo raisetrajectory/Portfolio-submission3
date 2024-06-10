0bd7ef6aaf7dd5c67240ad3354e48e8b88af499c
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-10 16:57:38 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -127,19 +127,34 @@ class CustomAuthenticationForm(AuthenticationForm):
 #         model = ChatMessage
 #         fields = ['message', 'session']  # session フィールドを追加
 
-class ChatMessageForm(forms.ModelForm): #2024年6月10日追加
-    session_id = forms.IntegerField(widget=forms.HiddenInput())
+# class ChatMessageForm(forms.ModelForm): #2024年6月10日追加
+#     session_id = forms.IntegerField(widget=forms.HiddenInput())
 
+#     class Meta:
+#         model = ChatMessage
+#         fields = ['message', 'session_id']
+
+#     def __init__(self, *args, session_id=None, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['session_id'].initial = session_id
+#         self.fields['session_id'].widget = forms.HiddenInput()
+
+class ChatMessageForm(forms.ModelForm):
     class Meta:
         model = ChatMessage
-        fields = ['message', 'session_id']
+        fields = ['message']
 
     def __init__(self, *args, session_id=None, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['session_id'].initial = session_id
-        self.fields['session_id'].widget = forms.HiddenInput()
+        self.session_id = session_id
 
-
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if self.session_id:
+            instance.session_id = self.session_id
+        if commit:
+            instance.save()
+        return instance
 
 class CommentForm(forms.Form):
     message = forms.CharField(label='コメント', widget=forms.Textarea)
