852d02f3a6ce8fb6e083a27d7707e82ad9444dd3
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-12 08:45:21 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/main_app/models.py
+++ Portfolio-submission/online_counseling_platform/main_app/models.py
@@ -5,6 +5,41 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+# class ThemesManager(models.Manager): #2024年6月12日追加
+
+#     def fetch_all_themes(self):
+#         return self.order_by('id').all()
+
+# class Themes(models.Model): #2024年6月12日追加
+
+#     title = models.CharField(max_length=255)
+#     user = models.ForeignKey(
+#         'accounts.Users', on_delete=models.CASCADE
+#     )
+
+#     objects = ThemesManager()
+
+#     class Meta:
+#         db_table = 'themes'
+
+# class CommentsManager(models.Manager): #2024年6月12日追加
+#     def fetch_by_theme_id(self, theme_id):
+#         return self.filter(theme_id=theme_id).order_by('id').all()
+
+# class Comments(models.Model): #2024年6月12日追加
+
+#     comment = models.CharField(max_length=1000)
+#     user = models.ForeignKey(
+#         'accounts.Users', on_delete=models.CASCADE
+#     )
+#     theme = models.ForeignKey(
+#         'Themes', on_delete=models.CASCADE
+#     )
+#     objects = CommentsManager()
+
+#     class Meta:
+#         db_table = 'comments'
+
 class User(AbstractUser):
     is_counselor = models.BooleanField(default=False)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
