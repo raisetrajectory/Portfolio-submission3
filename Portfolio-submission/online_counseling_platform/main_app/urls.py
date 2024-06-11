d9fcfc7d1967b140e5f0e9e93e340eccc1631386
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-11 20:06:55 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/main_app/urls.py
+++ Portfolio-submission/online_counseling_platform/main_app/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'main_app'
+
+urlpatterns = [
+    path('create_theme/', views.create_theme, name='create_theme'), # type: ignore
+    path('list_themes/', views.list_themes, name='list_themes'), # type: ignore
+    path('edit_theme/<int:id>/', views.edit_theme, name='edit_theme'), # type: ignore
+    path('delete_theme/<int:id>/', views.delete_theme, name='delete_theme'), # type: ignore
+    path('post_comments/<int:theme_id>/', views.post_comments, name='post_comments'), # type: ignore
+    path('save_comment/', views.save_comment, name='save_comment'), # type: ignore
+]
