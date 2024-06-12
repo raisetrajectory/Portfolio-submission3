15c774161a18172ceb8c666a7fc29d89af8792b1
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-12 19:01:06 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/online_counseling_platform/urls.py
+++ Portfolio-submission/online_counseling_platform/online_counseling_platform/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
-from counseling.views import show_error_page # type: ignore #2024年6月12日追加
+from counseling.views import show_error_page  #6月12日追加
 
 # app_name = main_app #2024年6月12日追加
 
@@ -15,7 +15,7 @@ urlpatterns = [
     path('counseling/', include('counseling.urls')),
     path('main_app/', include('main_app.urls')),
 ]
-handler404 = show_error_page #2024年6月12日追加
+handler404 = show_error_page #6月12日追加
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
