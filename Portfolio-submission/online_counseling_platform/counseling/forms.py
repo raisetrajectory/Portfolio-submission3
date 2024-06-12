7e36e62cfc7244b6272f18160f51ecabfedde54e
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-12 10:35:43 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -41,6 +41,29 @@ from django.contrib.auth.password_validation import validate_password #6月12日
 #         model = Users
 #         fields = ('username', 'age', 'email', 'picture')
 
+# class PasswordChangeForm(forms.ModelForm): #6月12日追加
+
+#     password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+#     confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
+
+#     class Meta():
+#         model = Users
+#         fields = ('password', )
+
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         password = cleaned_data['password']
+#         confirm_password = cleaned_data['confirm_password']
+#         if password != confirm_password:
+#             raise forms.ValidationError('パスワードが異なります')
+
+#     def save(self, commit=False):
+#         user = super().save(commit=False)
+#         validate_password(self.cleaned_data['password'], user)
+#         user.set_password(self.cleaned_data['password'])
+#         user.save()
+#         return user
+
 class LoginForm(forms.Form): #6月12日追加
     email = forms.CharField(label="メールアドレス")
     password = forms.CharField(label="パスワード", widget=forms.PasswordInput())
