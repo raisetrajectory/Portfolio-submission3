9aa9853421a63de56361ccb17c6104222e71f8a7
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-12 20:05:52 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/online_counseling_platform/settings.py
+++ Portfolio-submission/online_counseling_platform/online_counseling_platform/settings.py
@@ -88,21 +88,21 @@ TEMPLATES = [
     },
 ]
 
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         "DIRS": [TEMPLATE_DIR,],
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             "context_processors": [
-#                 "django.template.context_processors.debug",
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.contrib.messages.context_processors.messages",
-#             ],
-#         },
-#     },
-# ] #6月12日追加
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [TEMPLATE_DIR,],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+] #6月12日追加
 
 WSGI_APPLICATION = "online_counseling_platform.wsgi.application"
 
