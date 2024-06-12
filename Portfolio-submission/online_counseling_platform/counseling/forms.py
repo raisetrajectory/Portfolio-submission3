20eb8edf518060c16273cdfad0186ee4565db47d
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-12 14:34:38 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -45,51 +45,28 @@ class LoginForm(forms.Form): #6月12日追加
     email = forms.CharField(label="メールアドレス")
     password = forms.CharField(label="パスワード", widget=forms.PasswordInput())
 
-# class PasswordChangeForm(forms.ModelForm): #6月12日追加
+class PasswordChangeForm(forms.ModelForm): #6月12日追加
 
-#     password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
-#     confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
+    password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
 
-#     class Meta():
-#         model = Users
-#         fields = ('password', )
+    class Meta():
+        model = Users
+        fields = ('password', )
 
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         password = cleaned_data['password']
-#         confirm_password = cleaned_data['confirm_password']
-#         if password != confirm_password:
-#             raise forms.ValidationError('パスワードが異なります')
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data['password']
+        confirm_password = cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('パスワードが異なります')
 
-#     def save(self, commit=False):
-#         user = super().save(commit=False)
-#         validate_password(self.cleaned_data['password'], user)
-#         user.set_password(self.cleaned_data['password'])
-#         user.save()
-#         return user
-
-# class PasswordChangeForm(forms.ModelForm): #6月12日追加
-
-#     password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
-#     confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
-
-#     class Meta():
-#         model = Users
-#         fields = ('password', )
-
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         password = cleaned_data['password']
-#         confirm_password = cleaned_data['confirm_password']
-#         if password != confirm_password:
-#             raise forms.ValidationError('パスワードが異なります')
-
-#     def save(self, commit=False):
-#         user = super().save(commit=False)
-#         validate_password(self.cleaned_data['password'], user)
-#         user.set_password(self.cleaned_data['password'])
-#         user.save()
-#         return user
+    def save(self, commit=False):
+        user = super().save(commit=False)
+        validate_password(self.cleaned_data['password'], user)
+        user.set_password(self.cleaned_data['password'])
+        user.save()
+        return user
 
 
 class UploadFileForm(forms.Form): #2024年6月6日追加
