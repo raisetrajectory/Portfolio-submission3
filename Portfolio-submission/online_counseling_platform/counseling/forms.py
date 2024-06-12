37ff99112121fa3005f3a715b290b2af15af7903
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-12 10:44:24 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/forms.py
+++ Portfolio-submission/online_counseling_platform/counseling/forms.py
@@ -68,6 +68,30 @@ class LoginForm(forms.Form): #6月12日追加
     email = forms.CharField(label="メールアドレス")
     password = forms.CharField(label="パスワード", widget=forms.PasswordInput())
 
+# class PasswordChangeForm(forms.ModelForm):
+
+#     password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+#     confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
+
+#     class Meta():
+#         model = Users
+#         fields = ('password', )
+
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         password = cleaned_data['password']
+#         confirm_password = cleaned_data['confirm_password']
+#         if password != confirm_password:
+#             raise forms.ValidationError('パスワードが異なります')
+
+#     def save(self, commit=False):
+#         user = super().save(commit=False)
+#         validate_password(self.cleaned_data['password'], user)
+#         user.set_password(self.cleaned_data['password'])
+#         user.save()
+#         return user
+
+
 class UploadFileForm(forms.Form): #2024年6月6日追加
     upload_file = forms.ImageField(label='画像ファイルを選択してください')
 
