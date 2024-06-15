59c1616b7354ff87574e66c7448a347ef1b3ab3b
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-15 18:14:06 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/views.py
+++ Portfolio-submission/online_counseling_platform/counseling/views.py
@@ -45,13 +45,13 @@ def regist(request): # type: ignore #6月追加　遷移出来ています！
         }
     )
 
-def activate_user(request, token): #6月12日追加
+def activate_user(request, token): #6月追加
     user_activate_token = UserActivateTokens.objects.activate_user_by_token(token) # type: ignore
     return render(
         request, 'activate_user.html'
     )
 
-# def activate_user(request, token): #6月12日追加
+# def activate_user(request, token): #6月追加
 #     user_activate_token = UserActivateTokens.objects.activate_user_by_token(token) # type: ignore
 #     return render(
 #         request, 'counseling/activate_user.html'
