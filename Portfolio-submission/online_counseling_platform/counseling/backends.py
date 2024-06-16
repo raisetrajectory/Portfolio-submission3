dcc13f57291e72b05d77b78c36e187dfec171e66
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-16 16:57:40 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/backends.py
+++ Portfolio-submission/online_counseling_platform/counseling/backends.py
@@ -3,10 +3,24 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class EmailOrUsernameModelBackend(ModelBackend):
+# class EmailOrUsernameModelBackend(ModelBackend): #6月追加　記載内容のバックアップです！
+#     def authenticate(self, request, username=None, password=None, **kwargs):
+#         if username is None:
+#             username = kwargs.get(User.USERNAME_FIELD) # type: ignore
+#         if username is None or password is None:
+#             return
+#         try:
+#             user = User.objects.get(username=username)
+#         except User.DoesNotExist:
+#             try:
+#                 user = User.objects.get(email=username)
+#             except User.DoesNotExist:
+#                 return
+#         if user.check_password(password) and self.user_can_authenticate(user):
+#             return user
+
+class EmailOrUsernameModelBackend(ModelBackend): #6月追加
     def authenticate(self, request, username=None, password=None, **kwargs):
-        if username is None:
-            username = kwargs.get(User.USERNAME_FIELD) # type: ignore
         if username is None or password is None:
             return
         try:
@@ -18,16 +32,4 @@ class EmailOrUsernameModelBackend(ModelBackend):
                 return
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
-
-# class EmailOrUsernameModelBackend(ModelBackend): #6月追加
-#     def authenticate(self, request, username=None, password=None, **kwargs):
-#         UserModel = get_user_model()
-#         if username is None:
-#             username = kwargs.get(UserModel.USERNAME_FIELD)
-#         try:
-#             user = UserModel.objects.get(email=username)
-#         except UserModel.DoesNotExist:
-#             return None
-#         else:
-#             if user.check_password(password) and self.user_can_authenticate(user):
-#                 return user
+        return None
