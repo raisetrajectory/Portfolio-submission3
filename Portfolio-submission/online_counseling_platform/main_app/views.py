2a7a43d1c3e63227e7db3460cade513ee40d3667
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-16 12:39:41 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/main_app/views.py
+++ Portfolio-submission/online_counseling_platform/main_app/views.py
@@ -101,7 +101,7 @@ def post_comments(request, theme_id): #6月追加
         post_comment_form.instance.user = request.user
         post_comment_form.save()
         cache.delete(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}')
-        return redirect('post_comments', theme_id= theme_id)
+        return redirect('main_app:post_comments', theme_id= theme_id)
     return render(
         request, 'post_comments.html', context={
             'post_comment_form': post_comment_form,
