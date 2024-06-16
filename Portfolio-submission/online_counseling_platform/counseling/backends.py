18a27bcd4f0d68126bf68cfac623c73edfcc93e9
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-16 18:39:16 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/counseling/backends.py
+++ Portfolio-submission/online_counseling_platform/counseling/backends.py
@@ -3,24 +3,10 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# class EmailOrUsernameModelBackend(ModelBackend): #6月追加　記載内容のバックアップです！
-#     def authenticate(self, request, username=None, password=None, **kwargs):
-#         if username is None:
-#             username = kwargs.get(User.USERNAME_FIELD) # type: ignore
-#         if username is None or password is None:
-#             return
-#         try:
-#             user = User.objects.get(username=username)
-#         except User.DoesNotExist:
-#             try:
-#                 user = User.objects.get(email=username)
-#             except User.DoesNotExist:
-#                 return
-#         if user.check_password(password) and self.user_can_authenticate(user):
-#             return user
-
-class EmailOrUsernameModelBackend(ModelBackend): #6月追加
+class EmailOrUsernameModelBackend(ModelBackend): #6月追加　記載内容のバックアップです！
     def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get(User.USERNAME_FIELD) # type: ignore
         if username is None or password is None:
             return
         try:
@@ -32,4 +18,3 @@ class EmailOrUsernameModelBackend(ModelBackend): #6月追加
                 return
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
-        return None
