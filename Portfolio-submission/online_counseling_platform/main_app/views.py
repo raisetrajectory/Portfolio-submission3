086d372ec36f19745968683b76d2acbf931ef2ec
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-16 10:46:21 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform/main_app/views.py
+++ Portfolio-submission/online_counseling_platform/main_app/views.py
@@ -22,8 +22,7 @@ def create_theme(request): #6月追加
             create_theme_form.instance.user = request.user
             create_theme_form.save()
             messages.success(request, '掲示板を作成しました。')
-            # return redirect('list_themes')
-            return redirect('home')
+            return redirect('main_app:list_themes')
         return render(
         request, 'create_theme.html', context={
             'create_theme_form': create_theme_form,
