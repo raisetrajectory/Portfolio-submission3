b23113d1dd1aad6888bee4f5bc0f099264918e65
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-17 17:40:00 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/urls.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('delete_theme/<int:id>/', views.delete_theme, name='delete_theme'),
     path('post_comments/<int:theme_id>/', views.post_comments, name='post_comments'),
     path('save_comment/', views.save_comment, name='save_comment'),
-    path('counselor_list/', views.counselor_list, name='counselor_list'), # type: ignore
+    path('counselor_list/', views.counselor_list, name='counselor_list'),
+    path('counselor_profile/<int:counselor_id>/', views.counselor_profile, name='counselor_profile'),
 ]
