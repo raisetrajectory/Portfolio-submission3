08f297c929bb8ef6a932ed851a9a2223294fb524
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-19 08:47:46 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -101,17 +101,28 @@ def counselor_profile(request):
     counselors = Counselors.objects.all()
     return render(request, 'boards/counselor_profile.html', {'counselors': counselors})
 
-# class CommentDeleteView(DeleteView):
+# class CommentDeleteView(DeleteView):　#仮記載です！不要ならば削除しましょう！
 #     model = Comments
 #     template_name = 'delete_comment.html'
 #     success_url = reverse_lazy('boards:post_comments')
 
-# class BookDeleteView(DeleteView):
+# class BookDeleteView(DeleteView):　#仮記載です！不要ならば削除しましょう！
 #     model = Books
 #     template_name = 'delete_book.html'
 #     success_url = reverse_lazy('store:list_books')
 
-class CommentDeleteView(DeleteView): #仮記載
+class CommentDeleteView(DeleteView): #仮記載です！不要ならば削除しましょう！
     model = Comments
     template_name = 'boards/post_comments.html'
     success_url = reverse_lazy('boards:post_comments')
+
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comments, id=comment_id)
+    if request.user != comment.user:
+        raise Http404
+    if request.method == 'POST':
+        theme_id = comment.theme.id
+        comment.delete()
+        messages.success(request, 'コメントを削除しました。')
+        return redirect('boards:post_comments', theme_id=theme_id)
+    return render(request, 'boards/delete_comment.html', context={'comment': comment})
