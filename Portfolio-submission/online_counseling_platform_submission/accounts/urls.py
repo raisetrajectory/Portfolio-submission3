4de43dae56e7dd24c123381d1e93a461b8362828
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-18 13:35:57 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/urls.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/urls.py
@@ -3,17 +3,6 @@ from . import views
 
 app_name = 'accounts'
 
-# 下記の記載内容は成功しませんでした。7~15
-# urlpatterns = [
-#     path('', views.home, name='home'),
-#     path('regist', views.regist, name='regist'),
-#     path('activate_user/<uuid:token>', views.activate_user, name='activate_user'),
-#     path('user_login', views.user_login, name='user_login'),
-#     path('user_logout', views.user_logout, name='user_logout'),
-#     path('user_edit', views.user_edit, name='user_edit'),
-#     path('change_password', views.change_password, name='change_password'),
-# ]
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('regist/', views.regist, name='regist'),
