15da07a26cead997f3aa01a94d2952ed006b0a29
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-20 08:28:03 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -113,20 +113,8 @@ def delete_comment(request, comment_id):
         return redirect('boards:post_comments', theme_id=theme_id)
     return render(request, 'boards/delete_comment.html', context={'comment': comment})
 
-# def upload_sample(request):
-#     if request.method == 'POST' and request.FILES['upload_file']:
-#         # 送られたファイルの取り出し
-#         upload_file = request.FILES['upload_file']
-#         fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'picture'))  # ファイルを保存する
-#         file_path = os.path.join('upload', upload_file.name)
-#         file = fs.save(file_path, upload_file)
-#         uploaded_file_url = fs.url(file)
-#         return  render(request, 'boards/counselor_profile.html', context={
-#             'uploaded_file_url': uploaded_file_url
-#         })
-#     return render(request, 'boards/counselor_profile.html')
-
-
+def base(request):
+    return render(request, 'base.html')
 
 def upload_sample(request):
     if request.method == 'POST' and request.FILES['upload_file']:
@@ -139,3 +127,16 @@ def upload_sample(request):
             'uploaded_file_url': uploaded_file_url
         })
     return render(request, 'boards/counselor_profile.html')
+
+# def upload_sample(request):
+#     if request.method == 'POST' and request.FILES['upload_file']:
+#         # 送られたファイルの取り出し
+#         upload_file = request.FILES['upload_file']
+#         fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'picture'))  # ファイルを保存する
+#         file_path = os.path.join('upload', upload_file.name)
+#         file = fs.save(file_path, upload_file)
+#         uploaded_file_url = fs.url(file)
+#         return  render(request, 'boards/counselor_profile.html', context={
+#             'uploaded_file_url': uploaded_file_url
+#         })
+#     return render(request, 'boards/counselor_profile.html')
