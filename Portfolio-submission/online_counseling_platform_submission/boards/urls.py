fdfc7193de6c17f33ca1d079774ec74ca967b0a3
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-22 11:18:35 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/urls.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'boards'
 
 urlpatterns = [
-
     path('create_theme/', views.create_theme, name='create_theme'),
     path('list_themes/', views.list_themes, name='list_themes'),
     path('edit_theme/<int:id>/', views.edit_theme, name='edit_theme'),
@@ -19,5 +18,4 @@ urlpatterns = [
     path('upload_model_form/', views.upload_model_form, name='upload_model_form'),
 
     path('', views.home, name='home'), #不要となった場合は削除して大丈夫です！ # type: ignore
-    # path('', views.home_view, name='home'),
 ]
