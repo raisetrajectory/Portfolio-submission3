ba1203216fecc09b35c2da27913cb3701c4c2c4e
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-22 15:37:36 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -11,30 +11,6 @@ import os
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 
-from boards.forms import PostCommentForm
-
-def home(request):
-    themes = Themes.objects.all()
-    comments = Comments.objects.filter(themes=themes)   # 必要に応じてクエリを修正します
-    post_comment_form = PostCommentForm()
-    context = {
-        'themes': themes,
-        'comments': comments,
-        'post_comment_form': post_comment_form,
-    }
-    return render(request, 'accounts/home.html', context)
-
-# def home(request):
-#     themes = Themes.objects.all()
-#     comments = Comments.objects.filter(themes=themes)   # 必要に応じてクエリを修正します
-#     post_comment_form = PostCommentForm()
-#     context = {
-#         'themes': themes,
-#         'comments': comments,
-#         'post_comment_form': post_comment_form,
-#     }
-#     return render(request, 'accounts/home.html', context)
-
 def create_theme(request):
         create_theme_form = forms.CreateThemeForm(request.POST or None)
         if create_theme_form.is_valid():
