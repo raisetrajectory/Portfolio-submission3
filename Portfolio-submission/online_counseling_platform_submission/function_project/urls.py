2da947c153eb67ed316245ecb29a521949541722
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-23 07:39:27 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/urls.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     path('boards/', include('boards.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# Debugモードの時に静的ファイルとメディアファイルを提供する設定を追加
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 
 # カスタム404エラーページのハンドラーを設定
 handler404 = show_error_page
