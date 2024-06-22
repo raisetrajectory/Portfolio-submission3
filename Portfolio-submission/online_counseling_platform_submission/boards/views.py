ae36ba806ea9b015bd96d555c599d53436291ed1
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-22 11:55:43 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -13,28 +13,28 @@ from django.conf import settings
 
 from boards.forms import PostCommentForm
 
-def home(request):
-    themes = Themes.objects.all()
-    comments = Comments.objects.filter(themes=themes)   # 必要に応じてクエリを修正します
-    post_comment_form = PostCommentForm()
-    context = {
-        'themes': themes,
-        'comments': comments,
-        'post_comment_form': post_comment_form,
-        # 'post_comment_form': forms.PostCommentForm(),  # 必要なフォームを渡す
-    }
-    return render(request, 'accounts/home.html', context)
-
 # def home(request):
 #     themes = Themes.objects.all()
-#     comments_dict = {theme.id: Comments.objects.filter(theme=theme) for theme in themes} # type: ignore
+#     comments = Comments.objects.filter(themes=themes)   # 必要に応じてクエリを修正します
 #     post_comment_form = PostCommentForm()
 #     context = {
 #         'themes': themes,
-#         'comments_dict': comments_dict,
+#         'comments': comments,
 #         'post_comment_form': post_comment_form,
+#         # 'post_comment_form': forms.PostCommentForm(),  # 必要なフォームを渡す
 #     }
 #     return render(request, 'accounts/home.html', context)
+
+def home(request):
+    themes = Themes.objects.all()
+    comments_dict = {theme.id: Comments.objects.filter(theme=theme) for theme in themes} # type: ignore
+    post_comment_form = PostCommentForm()
+    context = {
+        'themes': themes,
+        'comments_dict': comments_dict,
+        'post_comment_form': post_comment_form,
+    }
+    return render(request, 'accounts/home.html', context)
 
 def create_theme(request):
         create_theme_form = forms.CreateThemeForm(request.POST or None)
