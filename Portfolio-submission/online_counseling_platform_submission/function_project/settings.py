a4b3d98eefac6862518ab037fa2fc87f7277aed4
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-24 17:54:11 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = ['127.0.0.1','localhost', '.pythonanywhere.com', 'Portfolio_subm
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = get_random_secret_key()
+
+
 # Application definition
 
 INSTALLED_APPS = [
