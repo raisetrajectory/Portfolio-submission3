2cc8fd65e23c5c8fe463d976c7ee8aeae40f649b
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-25 10:36:51 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/local_settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/local_settings.py
@@ -1,6 +1,7 @@
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'Portfoliosubmission3.pythonanywhere.com']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'PortfolioSubmission3.pythonanywhere.com'] #上記の内容がうまくいかなった場合の保険です！
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-cw&h(&866t*h$ns35@(6ifum4*9fcn%20ibdi0@_1_sp8ne@wi"
