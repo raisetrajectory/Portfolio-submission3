1b39d05bee09db16522751274ae86d41e0283e61
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-06-26 06:38:24 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/wsgi.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/wsgi.py
@@ -7,10 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+#記載内容のバックアップです！問題や不具合がある場合下記の記載内容に戻りましょう！
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "function_project.settings")
+
+# application = get_wsgi_application()
+
+import os
+import sys
+
+# Djangoプロジェクトのディレクトリを指定します
+path = '/home/uchiyamatakuro/Portfolio-submission3/Portfolio-submission/online_counseling_platform_submission/function_project'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Djangoの設定ファイルを指定します
+os.environ['DJANGO_SETTINGS_MODULE'] = 'function_project.settings'
+
+# WSGIアプリケーションを取得します
 from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "function_project.settings")
 
-application = get_wsgi_application()
