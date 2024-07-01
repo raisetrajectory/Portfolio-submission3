71cc3b72f62621ca03293afe3b6a8d3eddaeb838
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-01 11:48:47 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -17,23 +17,6 @@ from django.contrib import messages
 from .models import Comments
 from .forms import PostCommentForm
 
-# def edit_comment(request, comment_id):
-#     comment = get_object_or_404(Comments, id=comment_id)
-#     # Ensure only the owner of the comment can edit it
-#     if comment.user.id != request.user.id:
-#         raise Http404
-#     edit_comment_form = PostCommentForm(request.POST or None, instance=comment)
-#     if edit_comment_form.is_valid():
-#         edit_comment_form.save()
-#         messages.success(request, 'コメントを更新しました。')
-#         return redirect('boards:post_comments')  # Assuming redirect to a relevant view
-#     return render(
-#         request, 'boards/edit_comment.html', context={
-#             'edit_comment_form': edit_comment_form,
-#             'comment_id': comment_id,
-#         }
-#     )
-
 def edit_comment(request, comment_id):
     comment = get_object_or_404(Comments, id=comment_id)
     # Ensure only the owner of the comment can edit it
