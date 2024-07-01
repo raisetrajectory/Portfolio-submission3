ba55ff53d5e656d7b4bdb00725755466e74f1832
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-01 11:13:05 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -34,7 +34,7 @@ def edit_comment(request, comment_id):
         }
     )
 
-# def delete_comment(request, comment_id):
+# def edit_comment(request, comment_id):
 #     comment = get_object_or_404(Comments, id=comment_id)
 #     if request.user != comment.user:
 #         raise Http404
@@ -42,7 +42,7 @@ def edit_comment(request, comment_id):
 #         theme_id = comment.theme.id
 #         theme = get_object_or_404(Themes, id=theme_id)  # テーマが存在するか確認
 #         comment.delete()
-#         messages.success(request, 'コメントを削除しました。')
+#         messages.success(request, 'コメントを更新しました。')
 #         return redirect('boards:post_comments', theme_id=theme_id)
 #     return render(request, 'boards/delete_comment.html', context={'comment': comment})
 
