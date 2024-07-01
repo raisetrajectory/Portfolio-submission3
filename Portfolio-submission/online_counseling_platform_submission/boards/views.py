6ab0167d56cbc0b40c0b416d2bd3494a203e90d0
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-01 11:10:03 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -34,6 +34,18 @@ def edit_comment(request, comment_id):
         }
     )
 
+# def delete_comment(request, comment_id):
+#     comment = get_object_or_404(Comments, id=comment_id)
+#     if request.user != comment.user:
+#         raise Http404
+#     if request.method == 'POST':
+#         theme_id = comment.theme.id
+#         theme = get_object_or_404(Themes, id=theme_id)  # テーマが存在するか確認
+#         comment.delete()
+#         messages.success(request, 'コメントを削除しました。')
+#         return redirect('boards:post_comments', theme_id=theme_id)
+#     return render(request, 'boards/delete_comment.html', context={'comment': comment})
+
 def create_theme(request):
         create_theme_form = forms.CreateThemeForm(request.POST or None) # type: ignore
         if create_theme_form.is_valid():
