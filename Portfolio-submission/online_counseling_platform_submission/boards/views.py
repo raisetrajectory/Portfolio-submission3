4b935dd8f6c14f7d90c440f2dc4c99de0f2d7e65
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-01 09:49:35 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -14,7 +14,7 @@ from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostCommentForm
 
-# def edit_comment(request, comment_id):
+# def edit_comment(request, comment_id): #記載内容のバックアップです！
 #     comment = get_object_or_404(Comments, id=comment_id)
 
 #     # コメントの所有者であるかを確認する
@@ -34,11 +34,8 @@ from .forms import PostCommentForm
 
 def edit_comment(request, comment_id):
     comment = get_object_or_404(Comments, id=comment_id)
-
-    # ユーザーがコメントの所有者でない場合はアクセスを拒否する
     if request.user != comment.user:
         raise Http404
-
     if request.method == 'POST':
         form = PostCommentForm(request.POST, instance=comment)
         if form.is_valid():
@@ -49,6 +46,18 @@ def edit_comment(request, comment_id):
         form = PostCommentForm(instance=comment)
 
     return render(request, 'boards/edit_comment.html', {'form': form, 'comment': comment})
+
+# def delete_comment(request, comment_id):
+#     comment = get_object_or_404(Comments, id=comment_id)
+#     if request.user != comment.user:
+#         raise Http404
+#     if request.method == 'POST':
+#         theme_id = comment.theme.id
+#         theme = get_object_or_404(Themes, id=theme_id)  # テーマが存在するか確認
+#         comment.delete()
+#         messages.success(request, 'コメントを削除しました。')
+#         return redirect('boards:post_comments', theme_id=theme_id)
+#     return render(request, 'boards/delete_comment.html', context={'comment': comment})
 
 def create_theme(request):
         create_theme_form = forms.CreateThemeForm(request.POST or None) # type: ignore
