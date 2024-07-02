7a0742c1b9b23dc4e7165af044bb9f02560433bd
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-02 20:48:18 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/views.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/views.py
@@ -60,6 +60,12 @@ def user_login(request):
         }
     )
 
+# @login_required # type: ignore
+# def user_logout(request):
+#     logout(request)
+#     messages.success(request, 'ログアウトしました')
+#     return redirect('accounts:home')
+
 @login_required
 def user_logout(request):
     logout(request)
