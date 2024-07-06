9fd93b9dacb9f579491944375b0fa6d9ca809b83
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-06 17:48:39 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/models.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/models.py
@@ -8,23 +8,6 @@ from uuid import uuid4
 from datetime import datetime, timedelta
 from django.contrib.auth.models import UserManager
 
-# class Counselor(models.Model):
-#     counselorname = models.CharField(max_length=255)
-#     age = models.PositiveIntegerField()
-#     email = models.EmailField(max_length=255, unique=True)
-#     is_active = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-#     picture = models.FileField(null=True, upload_to='picture/')
-
-#     objects = models.Manager()
-#     objects = UserManager()
-
-#     COUNSELORNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['counselorname']
-
-#     class Meta:
-#         db_table = 'accounts.counselors'
-
 class Users(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255)
     age = models.PositiveIntegerField()
@@ -76,37 +59,3 @@ def publish_token(sender, instance, **kwargs):
     # メールでURLを送る方がよい
     print(f'http://127.0.0.1:8000/accounts/activate_user/{user_activate_token.token}')
 
-# class CounselorActivateTokensManager(models.Manager):
-
-#     def activate_counselor_by_token(self, token):
-#         counselor_activate_token = self.filter(
-#             token=token,
-#             expired_at__gte=datetime.now()
-#         ).first()
-#         if counselor_activate_token:
-#             counselor = counselor_activate_token.counselor
-#             counselor.is_active = True
-#             counselor.save()
-
-# class CounselorActivateTokens(models.Model):
-#     token = models.UUIDField(db_index=True)
-#     expired_at = models.DateTimeField()
-#     counselor = models.ForeignKey(
-#         'Counselors', on_delete=models.CASCADE
-#     )
-
-#     objects = CounselorActivateTokensManager() # type: ignore
-
-#     class Meta:
-#         db_table = 'counselor_activate_tokens'
-
-# @receiver(post_save, sender=Counselors)
-# def publish_counselor_token(sender, instance, **kwargs):
-#     if kwargs.get('created', False):  # 新規作成されたときのみトークンを発行
-#         token = str(uuid4())
-#         expired_at = datetime.now() + timedelta(days=1)
-#         counselor_activate_token = CounselorActivateTokens.objects.create(
-#             counselor=instance, token=token, expired_at=expired_at
-#         )
-#         # メールでURLを送る方がよい
-#         print(f'http://127.0.0.1:8000/accounts/activate_counselor/{counselor_activate_token.token}')
