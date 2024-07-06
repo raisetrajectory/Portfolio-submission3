851030c163cde6f455695e3345d0f3b6f02992d1
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-06 14:35:39 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/models.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/models.py
@@ -24,6 +24,22 @@ from django.contrib.auth.models import UserManager
 #     class Meta:
 #         db_table = 'counselors'
 
+# class Counselors(models.Model):
+#     counselorname = models.CharField(max_length=255)
+#     age = models.PositiveIntegerField()
+#     email = models.EmailField(max_length=255, unique=True)
+#     is_active = models.BooleanField(default=False)
+#     is_staff = models.BooleanField(default=False)
+#     picture = models.FileField(null=True, upload_to='picture/')
+
+#     objects = models.Manager()
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['counselorname']
+
+#     class Meta:
+#         db_table = 'counselors'
+
 class Users(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255)
     age = models.PositiveIntegerField()
