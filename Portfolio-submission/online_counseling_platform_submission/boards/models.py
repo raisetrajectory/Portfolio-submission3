c404433e31a8203e18cc99a65a59a2a7c942a060
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-06 19:42:05 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/models.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/models.py
@@ -11,6 +11,9 @@ from accounts.models import Users  # assuming Users model is defined in accounts
 # from django.contrib.auth.models import CounselorManager
 from django.contrib.auth.models import BaseUserManager
 
+class CounselorManager(BaseUserManager):
+    pass  # カスタムのマネージャー機能を実装する場合にここに追加
+
 class ThemesManager(models.Manager):
 
     def fetch_all_themes(self):
@@ -53,9 +56,6 @@ class Comments(models.Model):
 #     class Meta:
 #         db_table = 'counselors'
 
-class CounselorManager(BaseUserManager):
-    pass  # カスタムのマネージャー機能を実装する場合にここに追加
-
 class Counselors(models.Model):
     counselorname = models.CharField(max_length=255)
     age = models.PositiveIntegerField()
@@ -65,11 +65,11 @@ class Counselors(models.Model):
     picture = models.FileField(null=True, upload_to='picture/')
     users = models.ManyToManyField('accounts.Users',related_name='counselors')#一人のカウンセラーが複数のユーザーと関連付けられます。
 
-    objects = CounselorManager()  # カスタムマネージャーを指定する
+    # objects = CounselorManager()  # カスタムマネージャーを指定する
     # objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['counselorname']
+    # USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['counselorname']
 
     class Meta:
         db_table = 'counselors'
@@ -98,7 +98,7 @@ class Counselors(models.Model):
 #     class Meta:
 #         db_table = 'user_activate_tokens'
 
-# @receiver(post_save, sender=Users)
+# @receiver(post_save, sender=Counselors)
 # def publish_token(sender, instance, **kwargs):
 #     print(str(uuid4()))
 #     print(datetime.now() + timedelta(days=1))
