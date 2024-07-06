7d1092b1d2cbb0c6b7f20986d6c069aa3d38e071
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-06 15:59:58 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/models.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+# from django.contrib.auth.models import (
+#     AbstractBaseUser, PermissionsMixin
+# )
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from uuid import uuid4
+# from datetime import datetime, timedelta
+# from django.contrib.auth.models import UserManager
 
 class ThemesManager(models.Manager):
 
