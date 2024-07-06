e46c05fb22736ed5e2a257dfa584ada035384e5a
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-06 16:10:47 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/models.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/models.py
@@ -44,19 +44,8 @@ class Comments(models.Model):
         db_table = 'comments'
 
 class Counselors(models.Model):
-# class Counselors(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
-    # age = models.PositiveIntegerField()
-    # email = models.EmailField(max_length=255, unique=True)
-    # is_active = models.BooleanField(default=False)
-    # is_staff = models.BooleanField(default=False)
-    # picture = models.FileField(null=True, upload_to='picture/')
     user = models.ForeignKey('accounts.Users', on_delete=models.CASCADE)
-
-    # objects = UserManager()
-
-    # USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = ['name']
 
     class Meta:
         db_table = 'counselors'
@@ -68,8 +57,10 @@ class Counselors(models.Model):
 #     is_active = models.BooleanField(default=False)
 #     is_staff = models.BooleanField(default=False)
 #     picture = models.FileField(null=True, upload_to='picture/')
-
+    # user = models.ForeignKey('accounts.Users', on_delete=models.CASCADE)
+    
 #     objects = models.Manager()
+    # objects = UserManager()
 
 #     USERNAME_FIELD = 'email'
 #     REQUIRED_FIELDS = ['name']
