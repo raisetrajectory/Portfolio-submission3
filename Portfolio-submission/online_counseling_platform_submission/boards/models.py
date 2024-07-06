2ba0b564ab24cf16408ec9edb6a639da542bf96a
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-06 20:06:51 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/models.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/models.py
@@ -108,6 +108,16 @@ class Counselors(models.Model):
 #     # メールでURLを送る方がよい
 #     print(f'http://127.0.0.1:8000/accounts/activate_user/{user_activate_token.token}')
 
+# @receiver(post_save, sender=Users)
+# def publish_token(sender, instance, **kwargs):
+#     print(str(uuid4()))
+#     print(datetime.now() + timedelta(days=1))
+#     user_activate_token = UserActivateTokens.objects.create(
+#         user=instance, token=str(uuid4()), expired_at=datetime.now() + timedelta(days=1)
+#     )
+#     # メールでURLを送る方がよい
+#     print(f'http://127.0.0.1:8000/accounts/activate_user/{user_activate_token.token}')
+
 
 class User(models.Model):
     name = models.CharField(max_length=50)
