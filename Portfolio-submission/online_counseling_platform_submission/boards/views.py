cb218131e6141a8a7ebe8e70a8a4c8b106b1adf7
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-07 09:45:12 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -191,3 +191,29 @@ def upload_model_form(request):
     return render(request, 'boards/upload_model_form.html', context={
         'form': form, 'user': user
     })
+
+from django.shortcuts import render, redirect
+from . import forms
+from django.core.exceptions import ValidationError
+# from .models import UserActivateTokens
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# def regist(request):
+#     regist_form = forms.RegistForm(request.POST or None)
+#     if regist_form.is_valid():
+#         try:
+#             regist_form.save()
+#             return redirect('accounts:home')
+#         except ValidationError as e:
+#             regist_form.add_error('password', e)
+#     return render(
+#         request, 'accounts/regist.html', context={
+#             'regist_form': regist_form,
+#         }
+#     )
