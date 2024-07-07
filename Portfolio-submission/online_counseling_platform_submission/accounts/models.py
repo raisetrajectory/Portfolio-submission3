fa195908530c8efb5635daa6662ecbe72a554b91
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-07 14:16:18 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/models.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/models.py
@@ -20,6 +20,17 @@ from django.contrib.auth.models import UserManager
         # user.set_password(password)
         # user.save(using=self._db)
         # return user
+    # def create_user(self, username, email, password=True):
+        # if not email:
+            # raise ValueError('emailを入力してください')
+        # user = self.model(
+            # username = username,
+            # email = email,
+        # )
+        # user.set_password(password)
+        # user.save(using=self._db)
+        # return user
+
 
 class Users(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255)
