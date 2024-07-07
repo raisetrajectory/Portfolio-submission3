30a83fb99506df8836e664f18a68536d868bb23c
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-07 09:26:54 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/forms.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/forms.py
@@ -74,11 +74,11 @@ class CounselorLoginForm(forms.Form):
     email = forms.CharField(label="メールアドレス")
     password = forms.CharField(label="パスワード", widget=forms.PasswordInput())
 
-# class CounselorPasswordChangeForm(forms.ModelForm):
+class CounselorPasswordChangeForm(forms.ModelForm):
 
-#     password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
-#     confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
+    password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
 
-#     class Meta():
-#         model = Users
-#         fields = ('password', )
+    class Meta():
+        model = Counselors
+        fields = ('password', )
