54b68af09b3f863c9a210fc1cb862d30742982b4
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-08 00:57:01 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/views.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/views.py
@@ -33,6 +33,20 @@ def regist(request):
         }
     )
 
+# def counselor_profile(request):
+#     regist_form = forms.RegistForm(request.POST or None)
+#     if regist_form.is_valid():
+#         try:
+#             regist_form.save()
+#             return redirect('accounts:home')
+#         except ValidationError as e:
+#             regist_form.add_error('password', e)
+#     return render(
+#         request, 'accounts/regist.html', context={
+#             'regist_form': regist_form,
+#         }
+#     )
+
 def activate_user(request, token):
     user_activate_token = UserActivateTokens.objects.activate_user_by_token(token) # type: ignore
     return render(
