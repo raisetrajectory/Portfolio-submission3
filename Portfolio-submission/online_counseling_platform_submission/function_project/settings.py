ff7581a0bd15d60fc2aad34794fc9801e6ae4c70
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-07 19:06:47 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -132,6 +132,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',  # 標準の認証バックエンド
+    'accounts.backends.CustomBackend',  # カスタム認証バックエンドのパスを追加する
+]
+
+
 LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
