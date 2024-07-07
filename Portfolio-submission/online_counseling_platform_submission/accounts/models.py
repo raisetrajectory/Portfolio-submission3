75207aeed48f82408259d28c41e837b595924c8c
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-07 20:40:29 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/models.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/models.py
@@ -6,31 +6,31 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from uuid import uuid4
 from datetime import datetime, timedelta
-from django.contrib.auth.models import UserManager
+# from django.contrib.auth.models import UserManager
 
-# class UserManager(BaseUserManager): #このメソッドは、一般ユーザーを作成するためのものです。
-#     #ユーザー作成手法
-#     def create_user(self, username, email, password=True): #username と email を引数として受け取り、email が指定されていない場合は ValueError を発生させます。
-#         if not email:
-#             raise ValueError('emailを入力してください')
-#         user = self.model( #self.model を使用して新しいユーザーオブジェクトを作成し、username と email を設定します。
-#             username = username,
-#             email = email,
-#         )
-#         user.set_password(password) #set_password メソッドを使用してパスワードを設定し、ハッシュ化します。
-#         user.save(using=self._db) #save(using=self._db) を呼び出して、データベースにユーザーを保存します。
-#         return user
-#     def create_superuser(self, username, email, password=True): #このメソッドは、スーパーユーザー (管理者) を作成するためのものです。
-#         user = self.model( #username と email を引数として受け取り、create_user メソッドと同様にユーザーオブジェクトを作成します。
-#             username = username,
-#             email = email,
-#         )
-#         user.set_password(password) #set_password メソッドを使用してパスワードを設定し、ハッシュ化します。
-#         user.is_staff = True
-#         user.is_active = True
-#         user.is_superuser = True #is_staff、is_active、is_superuser のフラグをすべて True に設定して、スーパーユーザーの特権を付与します。
-#         user.save(using=self._db) #save(using=self._db) を呼び出して、データベースにユーザーを保存します。
-#         return user
+class UserManager(BaseUserManager): #このメソッドは、一般ユーザーを作成するためのものです。
+    #ユーザー作成手法
+    def create_user(self, username, email, password=True): #username と email を引数として受け取り、email が指定されていない場合は ValueError を発生させます。
+        if not email:
+            raise ValueError('emailを入力してください')
+        user = self.model( #self.model を使用して新しいユーザーオブジェクトを作成し、username と email を設定します。
+            username = username,
+            email = email,
+        )
+        user.set_password(password) #set_password メソッドを使用してパスワードを設定し、ハッシュ化します。
+        user.save(using=self._db) #save(using=self._db) を呼び出して、データベースにユーザーを保存します。
+        return user
+    def create_superuser(self, username, email, password=True): #このメソッドは、スーパーユーザー (管理者) を作成するためのものです。
+        user = self.model( #username と email を引数として受け取り、create_user メソッドと同様にユーザーオブジェクトを作成します。
+            username = username,
+            email = email,
+        )
+        user.set_password(password) #set_password メソッドを使用してパスワードを設定し、ハッシュ化します。
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True #is_staff、is_active、is_superuser のフラグをすべて True に設定して、スーパーユーザーの特権を付与します。
+        user.save(using=self._db) #save(using=self._db) を呼び出して、データベースにユーザーを保存します。
+        return user
 
 class Users(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255)
@@ -39,51 +39,51 @@ class Users(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     picture = models.FileField(null=True, upload_to='picture/')
-    # picture2 = models.ImageField(null=True, upload_to='images/')  # 新しい ImageField を追加します！
-    # introduction = models.CharField(max_length=255, null=True) #追加します!
-    # counselor = models.ForeignKey('Counselor', on_delete=models.SET_NULL, related_name='clients', null=True, blank=True)
+    picture2 = models.ImageField(null=True, upload_to='images/')  # 新しい ImageField を追加します！
+    introduction = models.CharField(max_length=255, null=True) #追加します!
+    counselor = models.ForeignKey('Counselor', on_delete=models.SET_NULL, related_name='clients', null=True, blank=True)
 
-    # groups =  models.ManyToManyField(
-        # Group,
-        # related_name='user_groups'
-    # )
-    # user_permissions = models.ManyToManyField(
-        # Permission,
-        # related_name='user_permissions'
-    # )
+    groups =  models.ManyToManyField(
+        Group,
+        related_name='user_groups'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='user_permissions'
+    )
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
-    # def __str__(self):
-        # return self.username
+    def __str__(self):
+        return self.username
 
     class Meta:
         db_table = 'users'
 
-# class Counselor(AbstractBaseUser, PermissionsMixin):
-#     username = models.CharField(max_length=255)
-#     age = models.PositiveIntegerField(default=0, null=True)
-#     email = models.EmailField(max_length=255, unique=True)
-#     is_active = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-#     picture = models.FileField(null=True, upload_to='picture/')
-#     picture2 = models.ImageField(null=True, upload_to='images/')  # 新しい ImageField を追加します！
-#     introduction = models.CharField(max_length=255, null=True)
-#     qualifications = models.CharField(max_length=255, null=True) #ユーザーの職業、学歴、専門知識などを記録するのに便利です。
+class Counselor(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255)
+    age = models.PositiveIntegerField(default=0, null=True)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    picture = models.FileField(null=True, upload_to='picture/')
+    picture2 = models.ImageField(null=True, upload_to='images/')  # 新しい ImageField を追加します！
+    introduction = models.CharField(max_length=255, null=True)
+    qualifications = models.CharField(max_length=255, null=True) #ユーザーの職業、学歴、専門知識などを記録するのに便利です。
 
-    # objects = UserManager()
+    objects = UserManager()
 
-    # USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
-    # def __str__(self):
-        # return self.username
+    def __str__(self):
+        return self.username
 
-    # class Meta:
-    #     db_table = 'counselor'
+    class Meta:
+        db_table = 'counselor'
 
 class UserActivateTokensManager(models.Manager):
 
