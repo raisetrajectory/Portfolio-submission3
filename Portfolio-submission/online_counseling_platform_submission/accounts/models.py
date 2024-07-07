163bdce2ff3ad6b2e29fa89c6d06fdbfb255e7f1
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-07 14:28:07 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/models.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractBaseUser, PermissionsMixin
+    AbstractBaseUser, PermissionsMixin,BaseUserManager,Group,Permission
 )
 from django.db.models.signals import post_save
 from django.dispatch import receiver
