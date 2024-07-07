52f163a9b4a76f021ae41678e05a3f592489b899
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-07 15:34:08 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/backends.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/backends.py
@@ -1,3 +1,7 @@
 # from django.contrib.auth import get_user_model
 # from django.core.exceptions import MultipleObjectsReturned
 # from .models import Users,Counselor
+
+# User = get_user_model()
+
+# class = CustonAuthBackend(BaseBackend):
