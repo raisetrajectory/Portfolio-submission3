9b611c8508f138dcb393bc2621eaaafb1b095782
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-08 17:11:14 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/admin.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+
 from .models import Users #不要となった場合は削除して大丈夫です
 # Register your models here.
 admin.site.register(Users) #不要となった場合は削除して大丈夫です
+
+# from django.contrib import Counselor
+# from .models import Counselor #不要となった場合は削除して大丈夫です
+# admin.site.register(Counselor) #不要となった場合は削除して大丈夫です
