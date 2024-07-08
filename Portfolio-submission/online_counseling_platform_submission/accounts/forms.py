3d1d1cb6513104596f77aea30e08c2f791c07240
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-08 10:05:59 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/forms.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/forms.py
@@ -94,3 +94,26 @@ class PasswordChangeForm(forms.ModelForm):
         user.save()
         return user
 
+class CounselorPasswordChangeForm(forms.ModelForm):
+
+    password = forms.CharField(label='新しいパスワード', widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='新しいパスワード再入力', widget=forms.PasswordInput())
+
+    class Meta:
+        model = Counselor
+        fields = ('password', )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data['password']
+        confirm_password = cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('パスワードが異なります')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        validate_password(self.cleaned_data['password'], user)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
