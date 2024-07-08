83ca92951280cc5d74ede952bc609830b45be146
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-08 09:26:31 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/forms.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/forms.py
@@ -28,6 +28,32 @@ class RegistForm(forms.ModelForm):
         user.save()
         return user
 
+class CounselorRegistForm(forms.ModelForm):
+    username = forms.CharField(label='ユーザーネーム')
+    age = forms.IntegerField(label='年齢', min_value=0)
+    email = forms.EmailField(label='メールアドレス')
+    password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
+
+    class Meta:
+        model = Counselor
+        fields = ('username', 'age', 'email', 'password')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data['password']
+        confirm_password = cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('パスワードが異なります')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        validate_password(self.cleaned_data['password'], user)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
 class UserEditForm(forms.ModelForm):
     username = forms.CharField(label='ユーザーネーム')
     age = forms.IntegerField(label='年齢', min_value=0)
