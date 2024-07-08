befa3b8d3d78d7e9152b95ef1fdd3c885fa71278
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-08 11:19:36 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/urls.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('counselor_regist/', views.counselor_regist, name='counselor_regist'),
     path('counselor_login/', views.counselor_login, name='counselor_login'),
     path('counselor_logout/', views.counselor_logout, name='counselor_logout'),
+    path('counselor_edit/', views.counselor_edit, name='counselor_edit'),
     path('activate_user/<uuid:token>/', views.activate_user, name='activate_user'),
     path('user_login/', views.user_login, name='user_login'),
     path('user_logout/', views.user_logout, name='user_logout'),
