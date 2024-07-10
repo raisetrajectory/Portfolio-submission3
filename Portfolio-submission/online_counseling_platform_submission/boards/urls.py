91407590a662487160c3e451d610df7356ea772e
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-10 18:05:17 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/urls.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/urls.py
@@ -5,7 +5,9 @@ app_name = 'boards'
 
 urlpatterns = [
     path('create_theme/', views.create_theme, name='create_theme'),
+    # path('counselor_create_theme/', views.create_theme, name='create_theme'),
     path('list_themes/', views.list_themes, name='list_themes'),
+    # path('counselor_list_themes/', views.list_themes, name='list_themes'),
     path('edit_theme/<int:id>/', views.edit_theme, name='edit_theme'),
     path('delete_theme/<int:id>/', views.delete_theme, name='delete_theme'),
     path('post_comments/<int:theme_id>/', views.post_comments, name='post_comments'),
