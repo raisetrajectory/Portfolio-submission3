8230402ccd30186a992968becfc9f22e8d85d7f6
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-12 04:53:58 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -382,47 +382,6 @@ from .models import Themes, Comments
 from .forms import PostCommentForm
 from .models import Counselor
 
-# @login_required
-# def post_comments(request, theme_id):
-#     saved_comment = cache.get(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}', '')
-#     post_comment_form = PostCommentForm(request.POST or None, initial={'comment': saved_comment})
-#     theme = get_object_or_404(Themes, id=theme_id)
-#     comments = Comments.objects.filter(theme_id=theme_id)
-
-#     if request.method == 'POST':
-#         if not request.user.is_authenticated:
-#             raise Http404
-
-#         # Save the comment
-#         comment = post_comment_form.save(commit=False)
-#         comment.theme = theme
-
-#         # Check if the logged-in user is a User or Counselor
-#         if hasattr(request.user, 'counselorprofile'):  # Counselor instance
-#             comment.counselor = request.user.counselorprofile
-#             comment.user = None
-#         elif hasattr(request.user, 'userprofile'):  # User instance
-#             comment.user = request.user.userprofile
-#             comment.counselor = None
-#         else:
-#             raise ValueError("User does not have a profile")
-
-#         comment.save()
-
-#         # Clear the saved comment from cache
-#         cache.delete(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}')
-
-#         # Redirect back to the post comments view
-#         messages.success(request, 'コメントが投稿されました。')
-#         return redirect('boards:post_comments', theme_id=theme.id) # type: ignore
-
-#     return render(
-#         request, 'boards/post_comments.html', context={
-#             'post_comment_form': post_comment_form,
-#             'theme': theme,
-#             'comments': comments,
-#         }
-#     )
 
 @login_required
 def post_comments(request, theme_id):
@@ -465,7 +424,6 @@ def post_comments(request, theme_id):
             'comments': comments,
         }
     )
-
 
 def save_comment(request):
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
