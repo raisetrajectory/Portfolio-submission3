d7675a4af960e893a9b416bbdd964ba2884955ad
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-11 16:43:18 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -351,23 +351,81 @@ def delete_theme(request, id):
     else:
         return redirect('accounts:home')
 
-def post_comments(request, theme_id): #記載内容のバックアップです！　この記載内容にもどれば大丈夫です！
-    saved_comment = cache.get(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}', '')
-    post_comment_form = forms.PostCommentForm(request.POST or None, initial={'comment': saved_comment})     # type: ignore
+# def post_comments(request, theme_id): #記載内容のバックアップです！　この記載内容にもどれば大丈夫です！
+#     saved_comment = cache.get(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}', '')
+#     post_comment_form = forms.PostCommentForm(request.POST or None, initial={'comment': saved_comment})     # type: ignore
+#     theme = get_object_or_404(Themes, id=theme_id)
+#     comments = Comments.objects.fetch_by_theme_id(theme_id) # type: ignore
+#     if post_comment_form.is_valid():
+#         if not request.user.is_authenticated:
+#             raise Http404
+#         post_comment_form.instance.theme = theme
+#         post_comment_form.instance.user = request.user
+#         post_comment_form.save()
+#         cache.delete(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}')
+#         return redirect('boards:post_comments', theme_id= theme_id)
+#     return render(
+#         request, 'boards/post_comments.html', context={
+#             'post_comment_form': post_comment_form,
+#             'theme': theme,
+#             'comments': comments,
+#         }
+#     )
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
+from django.http import Http404
+from django.contrib import messages
+from .models import Themes, Comments
+from .forms import PostCommentForm
+from accounts.models import Users, Counselor
+
+@login_required
+def post_comments(request, theme_id):
     theme = get_object_or_404(Themes, id=theme_id)
-    comments = Comments.objects.fetch_by_theme_id(theme_id) # type: ignore
+
+    # Check if the theme user is the current user or the current user is a counselor and has this user as a client
+    if theme.user != request.user:
+        if not (isinstance(request.user, Counselor) and theme.user in request.user.clients.all()):  # type: ignore
+            raise Http404
+
+    # Determine the user type
+    if isinstance(request.user, Users):
+        user_type = 'User'
+    elif isinstance(request.user, Counselor):
+        user_type = 'Counselor'
+    else:
+        return redirect('accounts:home')
+
+    # Get the saved comment from the cache
+    saved_comment = cache.get(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}', '') # type: ignore
+    post_comment_form = PostCommentForm(request.POST or None, initial={'comment': saved_comment})
+
+    # Fetch the comments for the theme
+    comments = Comments.objects.fetch_by_theme_id(theme_id)  # type: ignore
+
     if post_comment_form.is_valid():
         if not request.user.is_authenticated:
             raise Http404
+
+        # Save the comment
         post_comment_form.instance.theme = theme
         post_comment_form.instance.user = request.user
         post_comment_form.save()
-        cache.delete(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}')
-        return redirect('boards:post_comments', theme_id= theme_id)
+
+        # Clear the saved comment from the cache
+        cache.delete(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}') # type: ignore
+
+        # Redirect back to the post comments view
+        messages.success(request, 'コメントが投稿されました。')
+        return redirect('boards:post_comments', theme_id=theme.id) # type: ignore
+
     return render(
         request, 'boards/post_comments.html', context={
             'post_comment_form': post_comment_form,
             'theme': theme,
+            'user_type': user_type,
             'comments': comments,
         }
     )
