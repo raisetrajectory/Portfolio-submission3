60353adc263af35d77b42164454fdbf46c4f0bcc
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-14 04:05:55 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -703,14 +703,6 @@ def delete_theme(request, id):
 #         }
 #     )
 
-# from django.contrib.auth.decorators import login_required
-# from django.shortcuts import render, redirect, get_object_or_404
-# from django.http import Http404
-# from django.core.cache import cache
-# from django.contrib import messages
-# from .models import Themes, Comments
-# from .forms import PostCommentForm
-
 # @login_required #ユーザー側がログインした場合はコメントを入力出来ております！　問題や不具合が発生した場合この記載内容に戻りましょう！
 # def post_comments(request, theme_id):
 #     saved_comment = cache.get(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}', '')
