130ea1a95e62b9305bb8249bcba525e658215fb5
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-14 02:01:38 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -22,19 +22,19 @@ from django.contrib.auth.decorators import login_required
 from .models import Counselors
 from accounts.models import Counselor, Users
 
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import Themes
+# from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+# from .models import Themes
 
-@login_required
-def theme_list(request):
-    # ログインしているユーザーが作成していないテーマを取得
-    themes = Themes.objects.exclude(user=request.user)
-    # ログインしているユーザーが作成したテーマを取得
-    themes = Themes.objects.filter(user=request.user)
-    return render(request, 'boards/list_themes.html', {
-        'themes': themes,
-    })
+# @login_required
+# def theme_list(request):
+#     # ログインしているユーザーが作成していないテーマを取得
+#     themes = Themes.objects.exclude(user=request.user)
+#     # ログインしているユーザーが作成したテーマを取得
+#     themes = Themes.objects.filter(user=request.user)
+#     return render(request, 'boards/list_themes.html', {
+#         'themes': themes,
+#     })
 
 # from django.shortcuts import render
 # from django.contrib.auth.decorators import login_required
