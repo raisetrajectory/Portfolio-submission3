ca8b3c1df4d5bae342cef8cb8f9bb79810a1dc6a
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-14 13:22:17 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -473,46 +473,6 @@ def delete_theme(request, id):
 #         }
 #     )
 
-# @login_required #ユーザー側がログインした場合はコメントを入力出来ております！　問題や不具合が発生した場合この記載内容に戻りましょう！
-# def post_comments(request, theme_id):
-#     saved_comment = cache.get(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}', '')
-#     post_comment_form = PostCommentForm(request.POST or None, initial={'comment': saved_comment})
-#     theme = get_object_or_404(Themes, id=theme_id)
-#     comments = Comments.objects.filter(theme_id=theme_id)
-
-#     if request.method == 'POST':
-#         if not request.user.is_authenticated:
-#             raise Http404
-
-#         # Save the comment
-#         comment = post_comment_form.save(commit=False)
-#         comment.theme = theme
-
-#         # Set user or counselor based on the logged-in user
-#         if hasattr(request.user, 'counselor'):  # Counselor instance
-#             comment.counselor = request.user.counselor
-#             comment.user = request.user  # カウンセラーでもuserにカウンセラーのインスタンスをセット
-#         else:  # User instance
-#             comment.user = request.user
-#             comment.counselor = None
-
-#         comment.save()
-
-#         # Clear the saved comment from cache
-#         cache.delete(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}')
-
-#         # Redirect back to the post comments view
-#         messages.success(request, 'コメントが投稿されました。')
-#         return redirect('boards:post_comments', theme_id=theme.id) # type: ignore
-
-#     return render(
-#         request, 'boards/post_comments.html', context={
-#             'post_comment_form': post_comment_form,
-#             'theme': theme,
-#             'comments': comments,
-#         }
-#     )
-
 @login_required #ユーザー側がログインしている場合はコメント入力出来ます！ カウンセラー側がログインしている場合はコメント入力出来ます！
 def post_comments(request, theme_id):
     saved_comment = cache.get(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}', '')
