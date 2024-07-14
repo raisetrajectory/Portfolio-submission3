6b130224b6b936093848bce2c1952476bd01f433
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-14 11:39:36 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/admin.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/admin.py
@@ -3,3 +3,5 @@ from .models import Themes
 from .models import Themes, Comments, Counselors
 # Register your models here.
 admin.site.register(Themes)
+admin.site.register(Comments)
+admin.site.register(Counselors)
