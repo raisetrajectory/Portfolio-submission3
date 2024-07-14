a27d4bf123dfa7d204097eb4b98f1c2fdff32dc9
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-15 05:56:29 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/decorators.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/decorators.py
@@ -1,12 +1,12 @@
-# from django.contrib.auth.decorators import login_required
-# from django.core.exceptions import PermissionDenied
-# from .models import Counselor
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from .models import Counselor
 
-# def counselor_required(view_func):
-#     @login_required
-#     def _wrapped_view(request, *args, **kwargs):
-#         if isinstance(request.user, Counselor):
-#             return view_func(request, *args, **kwargs)
-#         else:
-#             raise PermissionDenied
-#     return _wrapped_view
+def counselor_required(view_func):
+    @login_required
+    def _wrapped_view(request, *args, **kwargs):
+        if isinstance(request.user, Counselor):
+            return view_func(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+    return _wrapped_view
