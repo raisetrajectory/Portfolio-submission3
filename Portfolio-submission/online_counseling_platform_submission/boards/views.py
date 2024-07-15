bb79a62421b06ca04af34a178939cfc897ca2ed8
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-15 15:23:04 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -397,27 +397,40 @@ def create_theme(request):
 #     else:
 #         return redirect('accounts:home')
 
-@login_required #ユーザー側がログインしてしても利用可能です！カウンセラー側がログインしても利用できます！ この記載内容に戻りましょう!
+# @login_required #ユーザー側がログインしてしても利用可能です！カウンセラー側がログインしても利用できます！ この記載内容に戻りましょう!
+# def list_themes(request):
+#     if request.user.is_authenticated:
+#         if isinstance(request.user, Users):
+#             user_type = 'User'
+#             user_instance = request.user
+#             themes = Themes.objects.fetch_all_themes() # type: ignore
+#             themes = Themes.objects.filter(user=user_instance)
+#             return render(request, 'boards/list_themes.html', {'themes': themes, 'user_type': user_type})
+#         elif isinstance(request.user, Counselor):
+#             user_type = 'Counselor'
+#             # カウンセラーが担当しているクライアントを取得する
+#             clients = request.user.clients.all()  # type: ignore # Adjust according to actual related nam
+#             themes = Themes.objects.filter(user__in=clients)
+#             return render(request, 'boards/list_themes.html', {'themes': themes, 'user_type': user_type})
+#         else:
+#             # その他の場合はリダイレクト
+#             return redirect('accounts:home')
+#     else:
+#         # ログインしていない場合はログインページにリダイレクト
+#         return redirect('accounts:home')
+
+@login_required
 def list_themes(request):
-    if request.user.is_authenticated:
-        if isinstance(request.user, Users):
-            user_type = 'User'
-            user_instance = request.user
-            themes = Themes.objects.fetch_all_themes() # type: ignore
-            themes = Themes.objects.filter(user=user_instance)
-            return render(request, 'boards/list_themes.html', {'themes': themes, 'user_type': user_type})
-        elif isinstance(request.user, Counselor):
-            user_type = 'Counselor'
-            # カウンセラーが担当しているクライアントを取得する
-            clients = request.user.clients.all()  # type: ignore # Adjust according to actual related nam
-            themes = Themes.objects.filter(user__in=clients)
-            return render(request, 'boards/list_themes.html', {'themes': themes, 'user_type': user_type})
-        else:
-            # その他の場合はリダイレクト
-            return redirect('accounts:home')
+    if isinstance(request.user, Users):
+        themes = Themes.objects.filter(user=request.user)
+    elif isinstance(request.user, Counselor):
+        themes = Themes.objects.filter(user__in=Users.objects.all())
     else:
-        # ログインしていない場合はログインページにリダイレクト
-        return redirect('accounts:home')
+        themes = Themes.objects.none()
+
+    return render(request, 'boards/list_themes.html', {
+        'themes': themes,
+    })
 
 @login_required #修正完了です！
 def edit_theme(request, id):
