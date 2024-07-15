09de07fbdad8776aafe0ce51c940ff8923d368c1
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-15 10:01:56 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/views.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/views.py
@@ -158,13 +158,11 @@ def user_edit(request):
 @login_required  # type: ignore #この記載内容に戻りましょう!
 def counselor_edit(request):
     counselor_edit_form = forms.CounselorEditForm(request.POST or None, request.FILES or None, instance=request.user)
-
     if request.method == 'POST':
         if counselor_edit_form.is_valid():
             counselor_edit_form.save()
             messages.success(request, '更新完了しました。')
             return redirect('accounts:counselor_edit')
-
     return render(request, 'accounts/counselor_edit.html', context={
         'counselor_edit_form': counselor_edit_form,
     })
