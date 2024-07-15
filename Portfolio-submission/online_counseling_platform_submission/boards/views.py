138c1082aaad14b04708dd718f967ea4f31ec086
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-15 17:19:19 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -528,7 +528,6 @@ def post_comments(request, theme_id):
         'comments': comments,
     })
 
-
 def save_comment(request):
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
         comment = request.GET.get('comment')
