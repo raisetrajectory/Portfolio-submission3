ea423ec032c8011b5b02a70306f27773be638e45
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-16 14:22:59 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -204,12 +204,45 @@ def list_themes(request):
 #     else:
 #         return redirect('accounts:home')
 
+# @login_required
+# def edit_theme(request, id):
+#     theme = get_object_or_404(Themes, id=id)
+
+#     # ユーザーがテーマの所有者であるか、またはカウンセラーでそのクライアントであるかを確認
+#     if not (theme.user == request.user or (isinstance(request.user, Counselor) and theme.user in request.user.clients.all())):  # type: ignore
+#         raise Http404
+
+#     if request.method == 'POST':
+#         edit_theme_form = CreateThemeForm(request.POST, instance=theme)
+#         if edit_theme_form.is_valid():
+#             edit_theme_form.save()
+#             messages.success(request, 'チャット画面を更新しました。')
+#             return redirect('boards:list_themes')
+#     else:
+#         edit_theme_form = CreateThemeForm(instance=theme)
+
+#     user_type = 'Counselor' if isinstance(request.user, Counselor) else 'User'
+
+#     return render(request, 'boards/edit_theme.html', context={
+#         'edit_theme_form': edit_theme_form,
+#         'user_type': user_type,
+#         'id': id,
+#         'theme': theme,  # テーマオブジェクトをテンプレートに渡す
+#     })
+
+from django.shortcuts import get_object_or_404, redirect
+from django.http import Http404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Themes, Users, Counselor
+from .forms import CreateThemeForm
+
 @login_required
 def edit_theme(request, id):
     theme = get_object_or_404(Themes, id=id)
 
     # ユーザーがテーマの所有者であるか、またはカウンセラーでそのクライアントであるかを確認
-    if not (theme.user == request.user or (isinstance(request.user, Counselor) and theme.user in request.user.clients.all())):  # type: ignore
+    if not (theme.user == request.user or (isinstance(request.user, Counselor) and theme.user.counselor == request.user)):
         raise Http404
 
     if request.method == 'POST':
@@ -229,7 +262,6 @@ def edit_theme(request, id):
         'id': id,
         'theme': theme,  # テーマオブジェクトをテンプレートに渡す
     })
-
 
 
 
