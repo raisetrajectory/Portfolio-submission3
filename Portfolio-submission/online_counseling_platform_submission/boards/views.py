3d3e18714ecc4e963526b7a88119391b7f0e797a
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-21 06:43:56 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -37,6 +37,37 @@ from .models import Counselor
 #         'themes': themes,
 #     })
 
+# from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+# from .models import Themes
+# from accounts.models import Users
+
+# @login_required
+# def theme_list(request):
+#     user = request.user
+
+#     if not user.is_counselor:
+#         # 一般ユーザーの場合、自分が作成したテーマのみを取得
+#         themes = Themes.objects.filter(user=user)
+#     else:
+#         # カウンセラーがログインしている場合
+#         if hasattr(user, 'counselor'):
+#             # カウンセラーが契約している利用者を取得
+#             contracted_users = Users.objects.filter(counselor=user.counselor)
+#             if contracted_users.exists():
+#                 # 契約している利用者が作成したテーマのみを取得
+#                 themes = Themes.objects.filter(user__in=contracted_users)
+#             else:
+#                 # 契約している利用者がいない場合は空のテーマリスト
+#                 themes = Themes.objects.none()
+#         else:
+#             # カウンセラーの情報がない場合は空のテーマリスト
+#             themes = Themes.objects.none()
+
+#     return render(request, 'boards/list_themes.html', {
+#         'themes': themes,
+#     })
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Themes
@@ -54,12 +85,8 @@ def theme_list(request):
         if hasattr(user, 'counselor'):
             # カウンセラーが契約している利用者を取得
             contracted_users = Users.objects.filter(counselor=user.counselor)
-            if contracted_users.exists():
-                # 契約している利用者が作成したテーマのみを取得
-                themes = Themes.objects.filter(user__in=contracted_users)
-            else:
-                # 契約している利用者がいない場合は空のテーマリスト
-                themes = Themes.objects.none()
+            # カウンセラーが契約している利用者のテーマを取得
+            themes = Themes.objects.filter(user__in=contracted_users)
         else:
             # カウンセラーの情報がない場合は空のテーマリスト
             themes = Themes.objects.none()
