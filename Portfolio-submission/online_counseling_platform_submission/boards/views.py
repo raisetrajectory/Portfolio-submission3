94eef8eb1f2c98c295f6f2912b06933f1d07dacf
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-21 14:38:40 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -37,26 +37,40 @@ from .models import Counselor
 #         'themes': themes,
 #     })
 
+# from django.contrib.auth.decorators import login_required
+# from django.shortcuts import render
+# from .models import Themes, Users, Counselor
+
+# @login_required
+# def theme_list(request):
+#     if isinstance(request.user, Counselor):
+#         # カウンセラーがログインしている場合
+#         # 自分が契約している利用者を取得
+#         users = Users.objects.filter(counselor=request.user)
+#         # 契約している利用者が作成したテーマのみを取得
+#         themes = Themes.objects.filter(user__in=users)
+#     elif isinstance(request.user, Users):
+#         # 一般ユーザーがログインしている場合
+#         themes = Themes.objects.filter(user=request.user)
+#     else:
+#         # その他の場合は空のテーマリスト
+#         themes = Themes.objects.none()
+
+#     return render(request, 'boards/list_themes.html', {
+#         'themes': themes,
+#     })
+
 @login_required
 def theme_list(request):
     if isinstance(request.user, Counselor):
-        # カウンセラーがログインしている場合
-        # 自分が契約している利用者が作成したテーマのみを取得
-        # カウンセラーに関連する利用者を取得
         users = Users.objects.filter(counselor=request.user)
-        # その利用者が作成したテーマを取得
         themes = Themes.objects.filter(user__in=users)
     elif isinstance(request.user, Users):
-        # 一般ユーザーがログインしている場合
         themes = Themes.objects.filter(user=request.user)
     else:
-        # その他の場合は空のテーマリスト
         themes = Themes.objects.none()
 
-    return render(request, 'boards/list_themes.html', {
-        'themes': themes,
-    })
-
+    return render(request, 'boards/list_themes.html', {'themes': themes})
 
 @login_required #記載内容のバックアップです！
 def counselor_list(request):
