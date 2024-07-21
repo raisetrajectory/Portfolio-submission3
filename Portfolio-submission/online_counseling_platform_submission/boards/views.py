bbb6cca047cd9246ed807142ba6897b85f6e3a8b
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-21 19:05:01 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -58,26 +58,7 @@ from .models import Counselor
 #         'themes': themes,
 #     })
 
-# @login_required #記載内容のバックアップです！
-# def theme_list(request):
-#     if hasattr(request.user, 'counselor'):
-#         # カウンセラーがログインしている場合
-#         # カウンセラーが契約している利用者を取得
-#         users = Users.objects.filter(counselor=request.user)
-#         # 契約している利用者が作成したテーマのみを取得
-#         themes = Themes.objects.filter(user__in=users).distinct()  # distinct() を追加
-#     elif hasattr(request.user, 'users'):
-#         # 一般ユーザーがログインしている場合
-#         themes = Themes.objects.filter(user=request.user)
-#     else:
-#         # その他の場合は空のテーマリスト
-#         themes = Themes.objects.none()
-
-#     return render(request, 'boards/list_themes.html', {
-#         'themes': themes,
-#     })
-
-@login_required
+@login_required #記載内容のバックアップです！
 def theme_list(request):
     if hasattr(request.user, 'counselor'):
         # カウンセラーがログインしている場合
