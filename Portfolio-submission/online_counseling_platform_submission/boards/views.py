b2cac6919aea8736f9cd4239a1c1a1fa2380dacc
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-07-21 12:55:51 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -37,30 +37,24 @@ from .models import Counselor
 #         'themes': themes,
 #     })
 
-# @login_required
-# def theme_list(request):
-#     if isinstance(request.user, Users):
-#         # 一般ユーザーの場合、自分が作成したテーマのみを取得
-#         themes = Themes.objects.filter(user=request.user)
-#     elif isinstance(request.user, Counselor):
-#         themes = Themes.objects.filter(user__in=Users.objects.all())
-#     else:
-#         # カウンセラーの情報がない場合は空のテーマリスト
-#         themes = Themes.objects.none()
 
-#     return render(request, 'boards/list_themes.html', {
-#         'themes': themes,
-#     })
+
+# from django.contrib.auth.decorators import login_required
+# from django.shortcuts import render
+# from .models import Themes, Users, Counselor
 
 # @login_required
 # def theme_list(request):
 #     if isinstance(request.user, Counselor):
-#         # 一般ユーザーの場合、自分が作成したテーマのみを取得
-#         themes = Themes.objects.filter(user__in=Users.objects.all())
+#         # カウンセラーがログインしている場合
+#         # 自分が関連する利用者が作成したテーマのみを取得
+#         users = Users.objects.filter(counselor=request.user)
+#         themes = Themes.objects.filter(user__in=users)
 #     elif isinstance(request.user, Users):
+#         # 一般ユーザーがログインしている場合
 #         themes = Themes.objects.filter(user=request.user)
 #     else:
-#         # カウンセラーの情報がない場合は空のテーマリスト
+#         # その他の場合は空のテーマリスト
 #         themes = Themes.objects.none()
 
 #     return render(request, 'boards/list_themes.html', {
@@ -75,8 +69,10 @@ from .models import Themes, Users, Counselor
 def theme_list(request):
     if isinstance(request.user, Counselor):
         # カウンセラーがログインしている場合
-        # 自分が関連する利用者が作成したテーマのみを取得
+        # 自分が契約している利用者が作成したテーマのみを取得
+        # カウンセラーに関連する利用者を取得
         users = Users.objects.filter(counselor=request.user)
+        # その利用者が作成したテーマを取得
         themes = Themes.objects.filter(user__in=users)
     elif isinstance(request.user, Users):
         # 一般ユーザーがログインしている場合
