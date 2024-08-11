5b0779545681bdba05b294cc60db73b86cb3be62
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-11 09:11:52 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -92,43 +92,20 @@ def counselor_list(request):
 #     messages.success(request, f'{counselor.username}さんがあなたのカウンセラーに選ばれました。')
 #     return (redirect('boards:list_themes'))
 
-# @login_required #既に他のカウンセラーと契約中です。契約を解除してから新しいカウンセラーを選択してください。と表示されます！ 記載内容のバックアップです！
-# def select_counselor(request, counselor_id):
-#     user = request.user
-#     counselor = get_object_or_404(Counselor, id=counselor_id)
-
-#     # ユーザーが既に契約中のカウンセラーがいる場合は契約を許可しない
-#     if user.counselor:
-#         messages.error(request, '既に他のカウンセラーと契約中です。契約を解除してから新しいカウンセラーを選択してください。')
-#         return redirect('boards:counselor_list')
-
-#     user.counselor = counselor
-#     user.save()
-#     messages.success(request, f'{counselor.username}さんがあなたのカウンセラーに選ばれました。')
-#     return redirect('boards:list_themes')
-
-@login_required
+@login_required #既に他のカウンセラーと契約中です。契約を解除してから新しいカウンセラーを選択してください。と表示されます！ 記載内容のバックアップです！
 def select_counselor(request, counselor_id):
     user = request.user
     counselor = get_object_or_404(Counselor, id=counselor_id)
 
-    # ユーザーがカウンセラーであり、かつチェックマークが外れている場合にエラーを出す
-    if user.is_counselor and not user.is_counselor:  # カウンセラーでない場合はこのブロックに入らない
-        messages.error(request, 'カウンセラーとしてログイン中ではありません。カウンセラーとしてログインしてから再度お試しください。')
-        return redirect('boards:counselor_list')
-
-    # ユーザーが既に契約中のカウンセラーがいる場合
+    # ユーザーが既に契約中のカウンセラーがいる場合は契約を許可しない
     if user.counselor:
         messages.error(request, '既に他のカウンセラーと契約中です。契約を解除してから新しいカウンセラーを選択してください。')
         return redirect('boards:counselor_list')
 
-    # カウンセラーを選択して契約
     user.counselor = counselor
     user.save()
     messages.success(request, f'{counselor.username}さんがあなたのカウンセラーに選ばれました。')
     return redirect('boards:list_themes')
-
-
 
 # @login_required
 # def select_counselor(request, counselor_id):
