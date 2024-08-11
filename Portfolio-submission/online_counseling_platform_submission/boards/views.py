cc968ebed88805f8bc12ff429a83957a24ed33c6
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-11 12:36:36 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/views.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/views.py
@@ -107,33 +107,13 @@ def counselor_list(request):
 #     messages.success(request, f'{counselor.username}さんがあなたのカウンセラーに選ばれました。')
 #     return redirect('boards:list_themes')
 
-# @login_required
-# def select_counselor(request, counselor_id):
-#     user = request.user
-#     counselor = get_object_or_404(Counselor, id=counselor_id)
-
-#     # ユーザーが既に契約中のカウンセラーがいる場合は契約を許可しない
-#     if hasattr(user, 'counselor') and user.counselor:
-#         messages.error(request, '既に他のカウンセラーと契約中です。契約を解除してから新しいカウンセラーを選択してください。')
-#         return redirect('boards:counselor_list')
-
-#     user.counselor = counselor
-#     user.save()
-#     messages.success(request, f'{counselor.username}さんがあなたのカウンセラーに選ばれました。')
-#     return redirect('boards:list_themes')
-
 @login_required
 def select_counselor(request, counselor_id):
     user = request.user
     counselor = get_object_or_404(Counselor, id=counselor_id)
 
-    # ユーザーがカウンセラーであるかどうかを確認
-    if hasattr(user, 'counselor') and user.counselor:
-        messages.error(request, 'カウンセラーとしてログイン中は他のカウンセラーを選択できません。')
-        return redirect('boards:counselor_list')
-
     # ユーザーが既に契約中のカウンセラーがいる場合は契約を許可しない
-    if user.counselor:
+    if hasattr(user, 'counselor') and user.counselor:
         messages.error(request, '既に他のカウンセラーと契約中です。契約を解除してから新しいカウンセラーを選択してください。')
         return redirect('boards:counselor_list')
 
@@ -141,7 +121,6 @@ def select_counselor(request, counselor_id):
     user.save()
     messages.success(request, f'{counselor.username}さんがあなたのカウンセラーに選ばれました。')
     return redirect('boards:list_themes')
-
 
 # @login_required
 # def select_counselor(request, counselor_id):
