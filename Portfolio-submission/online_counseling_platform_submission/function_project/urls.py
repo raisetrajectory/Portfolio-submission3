c8eecb29cf0cac71f30fae6269ab7dc5c349d84d
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-14 17:35:52 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/urls.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/urls.py
@@ -8,7 +8,6 @@ from django.shortcuts import redirect
 from django.conf.urls import handler404 #2024年8月14日追加
 from accounts.views import show_error_page #2024年8月14日追加
 
-from django.conf.urls import handler404 #2024年8月14日追加
 from accounts import views  # アプリ名を正確に指定してください #2024年8月14日追加
 
 urlpatterns = [
