22ac798b750e3eb3b9e7d6f29a420cfdf2e64a9e
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-15 08:32:41 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/urls.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/urls.py
@@ -9,6 +9,7 @@ from django.conf.urls import handler404 #2024年8月14日追加
 from accounts.views import show_error_page #2024年8月14日追加
 
 from accounts import views  # アプリ名を正確に指定してください #2024年8月14日追加
+from django.http import Http404 # 追記箇所
 
 urlpatterns = [
     path('', lambda request: redirect('accounts:user_login', permanent=False)),  # リダイレクト先を'user_login'に変更
