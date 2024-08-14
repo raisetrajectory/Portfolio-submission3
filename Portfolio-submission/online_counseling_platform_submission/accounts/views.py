0baaf108d306d6355c57be06766356c262e8febe
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-15 08:03:06 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/views.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/views.py
@@ -203,6 +203,3 @@ from django.conf.urls import handler404
 
 def show_error_page(request, exception):
     return render(request, 'templates/404.html', status=404)
-
-# def error_404page(request, exception):
-#     return render(request, 'templates/404.html', status=404)
