d6e26aae6cf231646eec88fabec66378a6d85f62
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-15 15:11:15 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/boards/urls.py
+++ Portfolio-submission/online_counseling_platform_submission/boards/urls.py
@@ -3,10 +3,11 @@ from . import views
 # from .views import theme_list #新規追加
 from django.conf.urls import handler404 #2024年8月14日追加
 from boards import views
+from accounts.views import show_error_page
 
 handler404 = 'accounts.views.show_error_page'
 
-# handler404 = show_error_page
+handler404 = show_error_page
 
 app_name = 'boards'
 
