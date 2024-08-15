1ccca303fa98cc582b9fea851554d64fae120e90
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-15 11:57:33 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/local_settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/local_settings.py
@@ -1,9 +1,9 @@
-DEBUG = True #Visual Studio Codeで制作している場合はDEBUG = Trueに設定しましょう！
-# DEBUG = False
+# DEBUG = True #Visual Studio Codeで制作している場合はDEBUG = Trueに設定しましょう！
+# # DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'Portfoliosubmission3.pythonanywhere.com']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'uchiyamatakuro.pythonanywhere.com'] #ユーザー名【uchiyamatakuro】
-# ALLOWED_HOSTS = ['uchiyamatakuro.pythonanywhere.com']
+# # ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'Portfoliosubmission3.pythonanywhere.com']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'uchiyamatakuro.pythonanywhere.com'] #ユーザー名【uchiyamatakuro】
+# # ALLOWED_HOSTS = ['uchiyamatakuro.pythonanywhere.com']
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cw&h(&866t*h$ns35@(6ifum4*9fcn%20ibdi0@_1_sp8ne@wi"
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = "django-insecure-cw&h(&866t*h$ns35@(6ifum4*9fcn%20ibdi0@_1_sp8ne@wi"
