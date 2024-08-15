cddacfeb46f7035a4669954ef81a9d1743816c31
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-15 16:36:14 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from xml.dom.minidom import Notation
+# from xml.dom.minidom import Notation
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
