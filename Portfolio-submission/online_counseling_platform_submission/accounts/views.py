f21bd89f3e89dcb8832f82f0cb424c5f03b4a0a6
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-15 14:58:18 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/views.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/views.py
@@ -203,6 +203,8 @@ def change_password(request):
 #         request, '404.html'
 #     )
 
-def show_error_page(request, exception):
-    return render(request, 'templates/404.html', status=404)
+# def show_error_page(request, exception):
+#     return render(request, 'templates/404.html', status=404)
 
+def show_error_page(request, exception):
+    return render(request, '404.html')
