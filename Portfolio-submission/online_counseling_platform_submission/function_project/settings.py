86602208e7fcf538f566ec3cba89cd614b7bb466
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-17 17:25:41 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -199,3 +199,7 @@ CLOUDINARY_STORAGE = {
     'API_KEY': '849992194399532',
     'API_SECRET': 'uxavdn8ARiCFCCluRFYB7zXTu0Q',
 }
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
