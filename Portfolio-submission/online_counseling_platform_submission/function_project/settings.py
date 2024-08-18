a87f0c0611f4ef72eed9f3123b93d62fd70115e7
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-18 10:57:22 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -200,9 +200,9 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': 'uxavdn8ARiCFCCluRFYB7zXTu0Q',
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage' #203~205をコメントアウトすれば、再び画像が表示される様になります！
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage' #203~205をコメントアウトすれば、再び画像が表示される様になります！
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage' #203~205をコメントアウトすれば、再び画像が表示される様になります！
 # MEDIA_URL = '/media/'
