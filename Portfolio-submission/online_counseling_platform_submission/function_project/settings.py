4bf896859a057534f7cb6f6316b03dfa0648e9f1
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-20 17:38:19 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 # from xml.dom.minidom import Notation
 
+# # Cloudinaryの設定
+# os.environ['CLOUDINARY_URL'] = 'cloudinary://849992194399532:uxavdn8ARiCFCCluRFYB7zXTu0Q@dfo6zpzik'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
