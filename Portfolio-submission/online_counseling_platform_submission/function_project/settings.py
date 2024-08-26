c6fb3cd150d2a73a841744eb5cad76e7897f3000
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-26 19:22:45 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -84,6 +84,9 @@ SESSION_COOKIE_SECURE = True
 #   secure=False  # HTTPで接続する設定
 # )
 
+# # Cloudinaryの設定
+# CLOUDINARY_URL = 'cloudinary://849992194399532:uxavdn8ARiCFCCluRFYB7zXTu0Q@dfo6zpzik'
+
 from django.core.management.utils import get_random_secret_key
 SECRET_KEY = get_random_secret_key()
 
