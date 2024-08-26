0795cf12c3c98d735a3d4e68fb41c0d0f86e0674
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-26 20:34:13 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/forms.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/forms.py
@@ -48,9 +48,9 @@ class UserEditForm(forms.ModelForm):
         model = Users
         fields = ('username', 'age', 'email', 'picture', 'picture2', 'introduction', 'counselor')
 
-# from cloudinary.forms import CloudinaryFileField
+# from cloudinary.forms import CloudinaryFileField　#2024年8月追加
 
-# class UserEditForm(forms.ModelForm):
+# class UserEditForm(forms.ModelForm):　#2024年8月追加
 #     username = forms.CharField(label='ユーザーネーム')
 #     age = forms.IntegerField(label='年齢', min_value=0)
 #     email = forms.EmailField(label='メールアドレス')
@@ -125,7 +125,7 @@ class CounselorEditForm(forms.ModelForm):
             instance.save()
         return instance
 
-# class CounselorEditForm(forms.ModelForm):
+# class CounselorEditForm(forms.ModelForm):　#2024年8月追加
 #     username = forms.CharField(label='カウンセラーネーム')
 #     age = forms.IntegerField(label='年齢', min_value=0)
 #     email = forms.EmailField(label='メールアドレス')
