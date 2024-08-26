83eb98d8050ce03f0d7593712cbd75c7e01cf9e6
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-26 19:44:45 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -98,7 +98,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    # 'cloudinary_storage',
     "django.contrib.staticfiles",
     'accounts',
     'boards',
