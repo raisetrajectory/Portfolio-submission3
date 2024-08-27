6bb8052c1df0dd293dd27234f634e64d811b33e6
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-27 19:01:11 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -33,7 +33,7 @@ DEBUG = False #Visual Studio Codeで制作している場合はDEBUG = Falseに�
 # DEBUG = True
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'Portfoliosubmission3.pythonanywhere.com']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'uchiyamatakuro.pythonanywhere.com', 'http://127.0.0.1:8000/accounts/user_login/', 'www.uchiyamatakuro.pythonanywhere.com'] #ユーザー名【uchiyamatakuro】
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'uchiyamatakuro.pythonanywhere.com', 'http://127.0.0.1:8000/accounts/user_login/'] #ユーザー名【uchiyamatakuro】
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
