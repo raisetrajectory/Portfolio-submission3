a3c7d79fff81331a0c63809b5d3b63b6bd063a9c
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-28 02:09:14 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/accounts/views.py
+++ Portfolio-submission/online_counseling_platform_submission/accounts/views.py
@@ -122,7 +122,35 @@ def user_login(request):
         }
     )
 
-def counselor_login(request): #記載内容のバックアップです!
+# def counselor_login(request): #記載内容のバックアップです!
+#     counselor_login_form = forms.CounselorLoginForm(request.POST or None)
+#     if request.method == 'POST':
+#         if counselor_login_form.is_valid():
+#             email = counselor_login_form.cleaned_data.get('email')
+#             password = counselor_login_form.cleaned_data.get('password')
+#             counselor = authenticate(request, email=email, password=password)
+#             if counselor:
+#                 if counselor.is_active:
+#                     login(request, counselor)
+#                     messages.success(request, 'ログイン完了しました。')
+#                     return redirect('accounts:home')
+#                     # return redirect('boards:counselor_list')
+#                 else:
+#                     messages.warning(request, 'カウンセラーがアクティブでありません')
+#             else:
+#                 messages.warning(request, 'カウンセラーのメールアドレスまたはパスワードが間違っています')
+#     return render(
+#         request, 'accounts/counselor_login.html', context={
+#             'counselor_login_form': counselor_login_form,
+#         }
+#     )
+
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from . import forms
+
+def counselor_login(request):
     counselor_login_form = forms.CounselorLoginForm(request.POST or None)
     if request.method == 'POST':
         if counselor_login_form.is_valid():
@@ -133,8 +161,9 @@ def counselor_login(request): #記載内容のバックアップです!
                 if counselor.is_active:
                     login(request, counselor)
                     messages.success(request, 'ログイン完了しました。')
+
+                    # ログイン後のリダイレクト先を指定するためにリダイレクト先をセッションに設定
                     return redirect('accounts:home')
-                    # return redirect('boards:counselor_list')
                 else:
                     messages.warning(request, 'カウンセラーがアクティブでありません')
             else:
@@ -144,6 +173,7 @@ def counselor_login(request): #記載内容のバックアップです!
             'counselor_login_form': counselor_login_form,
         }
     )
+
 
 # def counselor_login(request):
 #     counselor_login_form = forms.CounselorLoginForm(request.POST or None)
