c08b213bb030648af9efabfe423f48b22cdb144a
“内山拓朗_00000“ <dqmsldqh20324@gmail.com>
2024-08-27 19:05:35 +0900

    https://github.com/raisetrajectory/Portfolio-submission3

--- Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
+++ Portfolio-submission/online_counseling_platform_submission/function_project/settings.py
@@ -250,11 +250,11 @@ except:
 
 # SECURE_SSL_REDIRECT = False #今後は絶対に記載しません！
 
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': 'dfo6zpzik',
-#     'API_KEY': '849992194399532',
-#     'API_SECRET': 'uxavdn8ARiCFCCluRFYB7zXTu0Q',
-# }
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dfo6zpzik',
+    'API_KEY': '849992194399532',
+    'API_SECRET': 'uxavdn8ARiCFCCluRFYB7zXTu0Q',
+}
 
 # import requests
 
@@ -271,12 +271,12 @@ except:
 # # Cloudinaryの設定
 # CLOUDINARY_URL = 'cloudinary://849992194399532:uxavdn8ARiCFCCluRFYB7zXTu0Q@dfo6zpzik'
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dfo6zpzik',
-    'API_KEY': '849992194399532',
-    'API_SECRET': 'uxavdn8ARiCFCCluRFYB7zXTu0Q',
-    'API_PROXY': 'http://proxy.server:3128',  # プロキシ設定の追加
-} #Visual Studio Codeにおいて、この記載内容で画像を問題や不具合無く取得して表示出来ております！
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': 'dfo6zpzik',
+#     'API_KEY': '849992194399532',
+#     'API_SECRET': 'uxavdn8ARiCFCCluRFYB7zXTu0Q',
+#     'API_PROXY': 'http://proxy.server:3128',  # プロキシ設定の追加
+# } #Visual Studio Codeにおいて、この記載内容で画像を問題や不具合無く取得して表示出来ております！
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
